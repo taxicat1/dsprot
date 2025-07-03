16364e0186e7435cca4c72d97ceda86db310fe2c
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-07-03 14:06:08 -0400

    Whitespace

--- include/encryptor.h
+++ include/encryptor.h
@@ -14,26 +14,26 @@ void Encryptor_EndRange(u32 addr);
 // Encryption range macros
 #define ENCRYPTION_START(enc_key) \
 	asm {                             \
-		stmfd  sp!, {r0-r9};          \
-		mov    r0, #6;                \
-		add    r0, pc, r0, lsl #1;    \
-		bl     Encryptor_StartRange;  \
-		ldmia  sp!, {r0-r9};          \
-		b      @__encry_ ## enc_key;  \
-		dcd    0xEB000000 + enc_key;  \
+	    stmfd  sp!, {r0-r9};          \
+	    mov    r0, #6;                \
+	    add    r0, pc, r0, lsl #1;    \
+	    bl     Encryptor_StartRange;  \
+	    ldmia  sp!, {r0-r9};          \
+	    b      @__encry_ ## enc_key;  \
+	    dcd    0xEB000000 + enc_key;  \
 	@__encry_ ## enc_key:             \
 	}
 
 #define ENCRYPTION_END(enc_key) \
 	asm {                             \
-		b      @__decry_ ## enc_key;  \
-		dcd    0xEB000000 + enc_key;  \
+	    b      @__decry_ ## enc_key;  \
+	    dcd    0xEB000000 + enc_key;  \
 	@__decry_ ## enc_key:             \
-		stmfd  sp!, {r0};             \
-		mov    r0, pc;                \
-		sub    r0, r0, #20;           \
-		bl     Encryptor_EndRange;    \
-		ldmfd  sp!, {r0};             \
+	    stmfd  sp!, {r0};             \
+	    mov    r0, pc;                \
+	    sub    r0, r0, #20;           \
+	    bl     Encryptor_EndRange;    \
+	    ldmfd  sp!, {r0};             \
 	}
 
 #endif
