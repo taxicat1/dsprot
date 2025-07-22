82ab301d9afb50741cb1c5e6c89a9561216daf7b
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-07-22 17:57:48 -0400

    Compatibility support for old API

--- dsprot.h
+++ dsprot.h
@@ -16,6 +16,47 @@ extern u32 DSProt_DetectNotEmulator(void* callback, void* param);
 extern u32 DSProt_DetectDummy(void* callback, void* param);
 extern u32 DSProt_DetectNotDummy(void* callback, void* param);
 
+static u32 __DSProt_compatibility_wrapper(void* callback);
+
+
+static u32 __DSProt_compatibility_wrapper(void* callback) {
+	if (callback) {
+		((void (*)(void))callback)();
+	}
+	
+	return ~(u32)callback;
+}
+
+
+static inline u32 DSProt_DetectFlashcart_Old(void* callback) {
+	return DSProt_DetectFlashcart(__DSProt_compatibility_wrapper, callback) == ~(u32)callback;
+}
+
+
+static inline u32 DSProt_DetectNotFlashcart_Old(void* callback) {
+	return DSProt_DetectNotFlashcart(__DSProt_compatibility_wrapper, callback) == ~(u32)callback;
+}
+
+
+static inline u32 DSProt_DetectEmulator_Old(void* callback) {
+	return DSProt_DetectEmulator(__DSProt_compatibility_wrapper, callback) == ~(u32)callback;
+}
+
+
+static inline u32 DSProt_DetectNotEmulator_Old(void* callback) {
+	return DSProt_DetectNotEmulator(__DSProt_compatibility_wrapper, callback) == ~(u32)callback;
+}
+
+
+static inline u32 DSProt_DetectDummy_Old(void* callback) {
+	return DSProt_DetectDummy(__DSProt_compatibility_wrapper, callback) == ~(u32)callback;
+}
+
+
+static inline u32 DSProt_DetectNotDummy_Old(void* callback) {
+	return DSProt_DetectNotDummy(__DSProt_compatibility_wrapper, callback) == ~(u32)callback;
+}
+
 #ifdef __cplusplus
 }
 #endif
