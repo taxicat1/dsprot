5c768121633dd6bde9f63abcba881fe83e3a5bad
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-08-04 14:45:33 -0400

    Decompile Encryptor_EncryptFunction

--- src/encryptor.c
+++ src/encryptor.c
@@ -134,89 +134,42 @@ void* Encryptor_DecryptFunction(u32 obfs_key, void* obfs_func_addr, u32 obfs_siz
 }
 
 
-// This function sucks. https://decomp.me/scratch/I41ac
-// 
-// This *should* be identical to `Encryptor_DecryptFunction` with the extra step
-// of modifying the key, and calling the encryption function instead of decryption.
-// But for some reason, all the instructions are in a totally different order.
-// You can match the instruction order with some weird dummy lines, but then
-// the registers just never line up. Something very stupid is happening.
-// I suspect there is some sort of obfuscation that is being partially 
-// optimized out, leaving behind only strange register patterns.
 u32 Encryptor_EncryptFunction(u32 obfs_key, void* obfs_func_addr, u32 obfs_size) {
-#ifdef NONMATCHING
-	
-	u32    expanded_key[4];
+    u32    expanded_key[4];
 	u32    literal_obfs_offset;
-	u32    key;
-	u32    size;
-	void*  func_addr;
-	
-	literal_obfs_offset = (u32)&BSS + ENC_VAL_1;
-	
-	func_addr = obfs_func_addr;
-	
-	key = obfs_key;
-	key -= literal_obfs_offset;
-	key += (u32)func_addr >> 20;
-	
-	size = obfs_size;
-	size -= literal_obfs_offset;
-	
-	expanded_key[0] = key ^ size;
-	expanded_key[1] = ((key <<  8) | (key >> 24)) ^ size;
-	expanded_key[2] = ((key << 16) | (key >> 16)) ^ size;
-	expanded_key[3] = ((key << 24) | (key >>  8)) ^ size;
-	
-	func_addr -= ENC_VAL_1;
-	
+    u32    new_key;
+    u32    size;
+    void*  func_addr;
+    
+    literal_obfs_offset = (u32)&BSS + ENC_VAL_1;
+    
+    func_addr = obfs_func_addr;
+    
+    obfs_size = obfs_size - literal_obfs_offset;
+    size = obfs_size;
+    
+    obfs_key = obfs_key - literal_obfs_offset + ((u32)func_addr >> 20);
+    new_key = obfs_key;
+    
+ 	expanded_key[0] = new_key;
+    expanded_key[0] = size ^ expanded_key[0];
+    
+    expanded_key[1] = new_key >> 24;
+	expanded_key[1] |= new_key << 8;
+    expanded_key[1] = size ^ (u32)expanded_key[1];
+    
+    expanded_key[2] = new_key >> 16;
+    expanded_key[2] |= new_key << 16;
+    expanded_key[2] = size ^ (u32)expanded_key[2];
+    
+    expanded_key[3] = new_key >> 8;
+    expanded_key[3] |= new_key << 24;
+    expanded_key[3] = size ^ (u32)expanded_key[3];
+    
+    func_addr -= ENC_VAL_1;
+    
 	RC4_InitAndEncryptInstructions(&expanded_key[0], func_addr, func_addr, size);
 	clearDataAndInstructionCache(func_addr, size);
 	
-	return key + literal_obfs_offset;
-	
-#else /* NONMATCHING */
-	
-	// push {r3, r4, r5, r6, r7, lr}
-	asm {
-		sub sp, sp, #16
-	}
-	u32 bss_addr = (u32)&BSS; // ldr r3, [pc, #124]
-	asm {
-		mov r7, r0
-		add r4, bss_addr, #ENC_VAL_1
-		mov r6, r1
-		sub r0, r7, r4
-		add r7, r0, r6, lsr #20
-		mov r5, r2
-		mov r2, r7, lsr #24
-		sub r5, r5, r4
-		mov r0, r7, lsr #8
-		orr r3, r2, r7, lsl #8
-		mov r1, r7, lsr #16
-		orr r2, r1, r7, lsl #16
-		eor lr, r5, r3
-		eor ip, r7, r5
-		eor r3, r5, r2
-		sub r1, r6, #ENC_VAL_1
-		str r3, [sp, #8]
-		orr r0, r0, r7, lsl #24
-		str ip, [sp]
-		eor ip, r5, r0
-		add r0, sp, #0
-		mov r2, r1
-		mov r3, r5
-		str lr, [sp, #4]
-		str ip, [sp, #12]
-		bl RC4_InitAndEncryptInstructions
-		mov r1, r5
-		sub r0, r6, #ENC_VAL_1
-		bl clearDataAndInstructionCache
-		add r0, r7, r4
-		add sp, sp, #16
-	}
-	// pop {r3, r4, r5, r6, r7, pc}
-	// .word BSS
-	
-#endif /* NONMATCHING */
+	return new_key + literal_obfs_offset;
 }
