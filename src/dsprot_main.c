87b97f186ee37a3a28e83598f4c63ac203a86999
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-08-10 14:23:20 -0400

    Use bool

--- src/dsprot_main.c
+++ src/dsprot_main.c
@@ -28,9 +28,9 @@ u32 DSProt_DetectFlashcart(void* callback) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	u32   result;
+	BOOL  result;
 	
-	result = 0;
+	result = FALSE;
 	
 	func_queue[0] = (u32)&ROMTest_IsBad + DSP_OBFS_OFFSET;
 	func_queue[1] = 0;
@@ -48,12 +48,12 @@ u32 DSProt_DetectFlashcart(void* callback) {
 	
 	func_result_sum >>= 1;
 	if (func_result_sum) {
-		result = 1;
+		result = TRUE;
 	} else if (func_result_sum == 0) {
-		result = 0;
+		result = FALSE;
 	}
 	
-	if (callback != NULL && result != 0) {
+	if (callback != NULL && result) {
 		((VoidFunc)callback)();
 	}
 	
@@ -68,9 +68,9 @@ u32 DSProt_DetectNotFlashcart(void* callback) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	u32   result;
+	BOOL  result;
 	
-	result = 0;
+	result = FALSE;
 	
 	func_queue[0] = (u32)&ROMTest_IsBad + DSP_OBFS_OFFSET;
 	func_queue[1] = 0;
@@ -88,12 +88,12 @@ u32 DSProt_DetectNotFlashcart(void* callback) {
 	
 	func_result_sum >>= 1;
 	if (func_result_sum) {
-		result = 0;
+		result = FALSE;
 	} else if (func_result_sum == 0) {
-		result = 1;
+		result = TRUE;
 	}
 	
-	if (callback != NULL && result != 0) {
+	if (callback != NULL && result) {
 		((VoidFunc)callback)();
 	}
 	
@@ -108,9 +108,9 @@ u32 DSProt_DetectEmulator(void* callback) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	u32   result;
+	BOOL  result;
 	
-	result = 0;
+	result = FALSE;
 	
 	func_queue[0] = (u32)&MACOwner_IsBad + DSP_OBFS_OFFSET;
 	func_queue[1] = 0;
@@ -128,12 +128,12 @@ u32 DSProt_DetectEmulator(void* callback) {
 	
 	func_result_sum >>= 1;
 	if (func_result_sum) {
-		result = 1;
+		result = TRUE;
 	} else if (func_result_sum == 0) {
-		result = 0;
+		result = FALSE;
 	}
 	
-	if (callback != NULL && result != 0) {
+	if (callback != NULL && result) {
 		((VoidFunc)callback)();
 	}
 	
@@ -148,9 +148,9 @@ u32 DSProt_DetectNotEmulator(void* callback) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	u32   result;
+	BOOL  result;
 	
-	result = 0;
+	result = FALSE;
 	
 	func_queue[0] = (u32)&MACOwner_IsBad + DSP_OBFS_OFFSET;
 	func_queue[1] = 0;
@@ -168,12 +168,12 @@ u32 DSProt_DetectNotEmulator(void* callback) {
 	
 	func_result_sum >>= 1;
 	if (func_result_sum) {
-		result = 0;
+		result = FALSE;
 	} else if (func_result_sum == 0) {
-		result = 1;
+		result = TRUE;
 	}
 	
-	if (callback != NULL && result != 0) {
+	if (callback != NULL && result) {
 		((VoidFunc)callback)();
 	}
 	
@@ -188,9 +188,9 @@ u32 DSProt_DetectDummy(void* callback) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	u32   result;
+	BOOL  result;
 	
-	result = 0;
+	result = FALSE;
 	
 	// Not optimized out here due to the asm inlines produced by the encryption macros
 	func_queue[0] = 0;
@@ -208,12 +208,12 @@ u32 DSProt_DetectDummy(void* callback) {
 	
 	func_result_sum >>= 1;
 	if (func_result_sum) {
-		result = 1;
+		result = TRUE;
 	} else if (func_result_sum == 0) {
-		result = 0;
+		result = FALSE;
 	}
 	
-	if (callback != NULL && result != 0) {
+	if (callback != NULL && result) {
 		((VoidFunc)callback)();
 	}
 	
@@ -228,9 +228,9 @@ u32 DSProt_DetectNotDummy(void* callback) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	u32   result;
+	BOOL  result;
 	
-	result = 0;
+	result = FALSE;
 	
 	// Not optimized out here due to the asm inlines produced by the encryption macros
 	func_queue[0] = 0;
@@ -248,12 +248,12 @@ u32 DSProt_DetectNotDummy(void* callback) {
 	
 	func_result_sum >>= 1;
 	if (func_result_sum) {
-		result = 0;
+		result = FALSE;
 	} else if (func_result_sum == 0) {
-		result = 1;
+		result = TRUE;
 	}
 	
-	if (callback != NULL && result != 0) {
+	if (callback != NULL && result) {
 		((VoidFunc)callback)();
 	}
 	
