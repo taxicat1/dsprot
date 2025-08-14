f5aa66324a88a3e46943787f13b308d4969824d2
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-08-14 16:01:56 -0400

    Add comments

--- src/rom_test.c
+++ src/rom_test.c
@@ -62,7 +62,11 @@ u32 ROMTest_IsBad(DSProt_Ctx* ctx) {
 			u32   addr      = rom_addr;
 			s32   num_bytes = ROM_BLOCK_SIZE;
 			
-			// `device_size` is checked from the rom header and used to offset the address
+			// `device_size` is checked from the rom header and used to offset the address.
+			// This field is `x` for the size of the ROM as `128KB << x`
+			// 128KB = 2^17, hence the addition of 17 before shifting
+			// Therefore, this increases the address by the size of the ROM.
+			// The ROM should mirror when this happens.
 			device_size = ((const CARDRomHeader*)CARD_GetRomHeader())->device_size;
 			addr += (1 << (device_size + 17));
 			
@@ -208,7 +212,7 @@ u32 ROMTest_IsBad(DSProt_Ctx* ctx) {
 			s32   num_bytes = ROM_BLOCK_SIZE;
 			
 			// Another round of manual cartridge reading here
-			// It is exactly the same as the above block, but without the ROM header check
+			// It is exactly the same as the above block, but without adding the total size of the ROM
 			
 			u32         register_base_1;
 			REGType8v*  vnull;
@@ -525,7 +529,11 @@ u32 ROMTest_IsGood(DSProt_Ctx* ctx) {
 			u32   addr      = rom_addr;
 			s32   num_bytes = ROM_BLOCK_SIZE;
 			
-			// `device_size` is checked from the rom header and used to offset the address
+			// `device_size` is checked from the rom header and used to offset the address.
+			// This field is `x` for the size of the ROM as `128KB << x`
+			// 128KB = 2^17, hence the addition of 17 before shifting
+			// Therefore, this increases the address by the size of the ROM.
+			// The ROM should mirror when this happens.
 			device_size = ((const CARDRomHeader*)CARD_GetRomHeader())->device_size;
 			addr += (1 << (device_size + 17));
 			
@@ -671,7 +679,7 @@ u32 ROMTest_IsGood(DSProt_Ctx* ctx) {
 			s32   num_bytes = ROM_BLOCK_SIZE;
 			
 			// Another round of manual cartridge reading here
-			// It is exactly the same as the above block, but without the ROM header check
+			// It is exactly the same as the above block, but without adding the total size of the ROM
 			
 			u32         register_base_1;
 			REGType8v*  vnull;
