cc237f5d6da1bb67b092ebd346cf58d1c1653449
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-08-14 16:00:45 -0400

    Add comments

--- src/rom_test.c
+++ src/rom_test.c
@@ -58,7 +58,11 @@ u32 ROMTest_IsBad(void) {
 			int         i;
 			u8          device_size;
 			
-			// `device_size` is checked from the rom header and used to offset the address
+			// `device_size` is checked from the rom header and used to offset the address.
+			// This field is `x` for the size of the ROM as `128KB << x`
+			// 128KB = 2^17, hence the addition of 17 before shifting
+			// Therefore, this increases the address by the size of the ROM.
+			// The ROM should mirror when this happens.
 			device_size = ((const CARDRomHeader*)CARD_GetRomHeader())->device_size;
 			addr += (1 << (device_size + 17));
 			
@@ -204,7 +208,7 @@ u32 ROMTest_IsBad(void) {
 			s32   num_bytes = ROM_BLOCK_SIZE;
 			
 			// Another round of manual cartridge reading here
-			// It is exactly the same as the above block, but without the ROM header check
+			// It is exactly the same as the above block, but without adding the total size of the ROM
 			
 			u32         register_base_1;
 			REGType8v*  vnull;
