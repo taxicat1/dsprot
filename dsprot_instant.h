6d4827c09e7f4defca48e80c097ef8ecfd7ea2d6
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-08-25 19:33:50 -0400

    Document checksum bug

--- dsprot_instant.h
+++ dsprot_instant.h
@@ -24,6 +24,7 @@ static inline void* DSProt_CheckAndDetectAll(void* callback, void* param1, void*
 	i = 37;
 	func_data_checksum = 0;
 	do {
+		// BUG: the first 5 loops have invalid shifts, resulting in 0 instead of the rotated instruction
 		func_data_checksum ^= (*func_data_ptr >> i) | (*func_data_ptr << (32-i));
 		func_data_ptr++;
 	} while (--i);
