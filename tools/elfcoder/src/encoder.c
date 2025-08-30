fcdd4158bd77c64b84ea4646c692d2e256067438
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-08-30 19:30:50 -0400

    Use mapping symbols for encoding

--- tools/elfcoder/src/encoder.c
+++ tools/elfcoder/src/encoder.c
@@ -76,7 +76,7 @@ void Encode_Instruction(Encoding_Ctx* ctx, Instruction* ins, RC4_Ctx* rc4) {
 }
 
 
-void Encode_Relocation(const Instruction* encoded_instruction, Elf32_Rela* reloc) {
+void Encode_Relocation(Elf32_Rela* reloc) {
 	reloc->r_addend += ENC_VAL_1 + 8;
 }
 
@@ -136,6 +136,6 @@ void Decode_Instruction(Encoding_Ctx* ctx, Instruction* ins, RC4_Ctx* rc4) {
 }
 
 
-void Decode_Relocation(const Instruction* encoded_instruction, Elf32_Rela* reloc) {
+void Decode_Relocation(Elf32_Rela* reloc) {
 	reloc->r_addend -= ENC_VAL_1 + 8;
 }
