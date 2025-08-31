2d69b86e0cb3a99b126ba07c7992ef28e2bf601b
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-08-30 20:04:58 -0400

    Refactor MAC address

--- src/mac_owner.c
+++ src/mac_owner.c
@@ -8,12 +8,18 @@
 u32 MACOwner_IsBad(DSProt_Ctx* ctx);
 u32 MACOwner_IsGood(DSProt_Ctx* ctx);
 
-static const u8 bad_mac_addr[6] = {
-	// 00:09:BF:00:00:31 after bit flipping
-	0xFF, 0xF6, 0x40, 0xFF, 0xFF, 0xCE
-};
-
 #define MAC_ADDR_SIZE  (6)
+
+#define ENC_MAC_ADDR_BYTE  (0xFF)
+
+static const u8 bad_mac_addr[MAC_ADDR_SIZE] = {
+	0x00 ^ ENC_MAC_ADDR_BYTE,
+	0x09 ^ ENC_MAC_ADDR_BYTE,
+	0xBF ^ ENC_MAC_ADDR_BYTE,
+	0x00 ^ ENC_MAC_ADDR_BYTE,
+	0x00 ^ ENC_MAC_ADDR_BYTE,
+	0x31 ^ ENC_MAC_ADDR_BYTE
+};
 
 
 u32 MACOwner_IsBad(DSProt_Ctx* ctx) {
@@ -24,7 +30,7 @@ u32 MACOwner_IsBad(DSProt_Ctx* ctx) {
 	
 	OS_GetMacAddress(&mac_addr[0]);
 	for (i = 0; i < MAC_ADDR_SIZE; i++) {
-		if (bad_mac_addr[i] != (mac_addr[i] ^ 0xFF)) {
+		if (bad_mac_addr[i] != (mac_addr[i] ^ ENC_MAC_ADDR_BYTE)) {
 			break;
 		}
 	}
@@ -66,7 +72,7 @@ u32 MACOwner_IsGood(DSProt_Ctx* ctx) {
 	
 	OS_GetMacAddress(&mac_addr[0]);
 	for (i = 0; i < MAC_ADDR_SIZE; i++) {
-		if (bad_mac_addr[i] != (mac_addr[i] ^ 0xFF)) {
+		if (bad_mac_addr[i] != (mac_addr[i] ^ ENC_MAC_ADDR_BYTE)) {
 			break;
 		}
 	}
