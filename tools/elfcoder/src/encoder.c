c520405df406118daf1e69205b09ba21994ab496
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-08 22:06:18 -0400

    Remove dependence on structure packing

--- tools/elfcoder/src/encoder.c
+++ tools/elfcoder/src/encoder.c
@@ -59,8 +59,10 @@ void Encode_Instruction(Encoding_Ctx* ctx, Instruction* ins, RC4_Ctx* rc4) {
 			
 			case 0:
 			case 3:
-				uint32_t original = ins->raw;
-				ins->raw ^= ctx->xor_val;
+				uint32_t ins_raw = Instruction_GetFull(ins);
+				uint32_t original = ins_raw;
+				ins_raw ^= ctx->xor_val;
+				Instruction_SetFull(ins, ins_raw);
 				ctx->xor_val <<= 1;
 				ctx->xor_val += original;
 				ctx->xor_val &= 0x00FFFFFF;
@@ -121,9 +123,11 @@ void Decode_Instruction(Encoding_Ctx* ctx, Instruction* ins, RC4_Ctx* rc4) {
 				ins->opcode ^= ENC_OPCODE_1;
 				// Fall through
 			case 0:
-				ins->raw ^= ctx->xor_val;
+				uint32_t ins_raw = Instruction_GetFull(ins);
+				ins_raw ^= ctx->xor_val;
+				Instruction_SetFull(ins, ins_raw);
 				ctx->xor_val <<= 1;
-				ctx->xor_val += ins->raw;
+				ctx->xor_val += ins_raw;
 				ctx->xor_val &= 0x00FFFFFF;
 				break;
 		}
