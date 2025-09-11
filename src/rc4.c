1f2e1051e4089fada56d3a57173f9d8ae2b1a57c
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-11 01:21:48 -0400

    Enums/defines for encryption constants

--- src/rc4.c
+++ src/rc4.c
@@ -46,25 +46,32 @@ typedef u32 (*FuncType_RC4_DecryptInstructions)(RC4_Ctx*, void*, void*, u32);
 typedef u32 (*FuncType_RC4_CategorizeInstruction)(u32);
 typedef u32 (*FuncType_RC4_Init)(RC4_Ctx*, void*, u32);
 
+enum {
+	INS_TYPE_OTHER = 0,
+	INS_TYPE_BLX,
+	INS_TYPE_BL,
+	INS_TYPE_B
+};
 
-u32 RC4_CategorizeInstruction(u32 instruction) {
+
+static u32 RC4_CategorizeInstruction(u32 instruction) {
 	u8 upper_byte;
 	
 	upper_byte = (instruction >> 24) & 0xFF;
 	
 	if ((upper_byte & 0x0E) == 0x0A) {
 		if ((upper_byte & 0xF0) == 0xF0) {
-			return 1;
+			return INS_TYPE_BLX;
 		}
 		
 		if (upper_byte & 0x01) {
-			return 2;
+			return INS_TYPE_BL;
 		} else {
-			return 3;
+			return INS_TYPE_B;
 		}
 	}
 	
-	return 0;
+	return INS_TYPE_OTHER;
 }
 
 
@@ -82,7 +89,7 @@ void RC4_Init(RC4_Ctx* ctx, const void* key, u32 key_len) {
 	// Must be like this to match
 	Si = Ki = 0;
 	
-	ctx->x = 0xAA;
+	ctx->x = ENC_RC4_X_START;
 	ctx->i = 0;
 	ctx->j = 0;
 	
@@ -166,11 +173,11 @@ u32 RC4_EncryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 		ins_word = *(u32*)(src_bytes + idx);
 		
 		switch (((FuncType_RC4_CategorizeInstruction)(Proxy_RC4_CategorizeInstruction - ENC_VAL_1))(ins_word)) {
-			case 1:
+			case INS_TYPE_BLX:
 				// Error correction (this case should never be run)
 				src_bytes[idx+3] ^= ENC_OPCODE_1;
 				// Fall through
-			case 2:
+			case INS_TYPE_BL:
 				*(u32*)(dst + idx) = *(u32*)(src_bytes + idx);
 				
 				upper = ((*(u32*)(dst + idx) & 0xFF000000) ^ (ENC_OPCODE_1 << 24));
@@ -179,7 +186,7 @@ u32 RC4_EncryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 				*(u32*)(dst_bytes + idx) = upper | lower;
 				break;
 			
-			case 3:
+			case INS_TYPE_B:
 				// Link bit
 				*(u32*)(src_bytes + idx) ^= (ENC_OPCODE_1 << 24);
 				// Fall through
@@ -258,17 +265,17 @@ u32 RC4_DecryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 		ins_word = *(u32*)(src_bytes + idx);
 		
 		switch (((FuncType_RC4_CategorizeInstruction)(Proxy_RC4_CategorizeInstruction - ENC_VAL_1))(ins_word)) {
-			case 1:
+			case INS_TYPE_BLX:
 				// Error correction (this case should never run)
 				src_bytes[idx+3] ^= ENC_OPCODE_1;
 				// Fall through
-			case 3:
+			case INS_TYPE_B:
 				*(u32*)(dst + idx) = ((ins_word & 0xFF000000) ^ (ENC_OPCODE_1 << 24)) |
 				                     (((ins_word & 0x00FFFFFF) - ENC_VAL_2) & 0x00FFFFFF);
 				
 				break;
 			
-			case 2:
+			case INS_TYPE_BL:
 				rc4_byte_addr = Proxy_RC4_Byte;
 				rc4_byte_addr -= ENC_VAL_1;
 				
