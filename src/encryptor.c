14bd9706392c66b699fae0e2a5ee82deca59dce5
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-13 02:37:46 -0400

    Improve function encoding decomp

--- src/encryptor.c
+++ src/encryptor.c
@@ -52,11 +52,11 @@ u32 Encryptor_CategorizeInstruction(u32 instruction) {
 
 
 void Encryptor_DecodeFunctionTable(FuncInfo* functions) {
-	u32  size;
-	u32  end_addr;
-	u32  xorval;
-	u32  bss_addr;
-	u32  addr;
+	u32   size;
+	u32*  end_addr;
+	u32   xorval;
+	u32   bss_addr;
+	u32*  addr;
 	
 	if (functions == NULL || functions->start_addr == NULL) {
 		return;
@@ -73,29 +73,29 @@ void Encryptor_DecodeFunctionTable(FuncInfo* functions) {
 			return;
 		}
 		
-		addr = (u32)(functions->start_addr - ENC_VAL_1);
-		end_addr = addr + (size & ~3);
+		addr = functions->start_addr - ENC_VAL_1;
+		end_addr = addr + (size / 4);
 		
-		for (; addr < end_addr; addr += 4) {
-			switch (Encryptor_CategorizeInstruction(*(u32*)addr)) {
+		for (; addr < end_addr; addr++) {
+			switch (Encryptor_CategorizeInstruction(*addr)) {
 				case INS_TYPE_BLX:
 				case INS_TYPE_B:
-					*(u32*)addr = ((*(u32*)addr & 0xFF000000) ^ (ENC_OPCODE_1 << 24)) |
-					              (((*(u32*)addr & 0x00FFFFFF) - ENC_VAL_2) & 0x00FFFFFF);
+					*addr = ((*addr & 0xFF000000) ^ (ENC_OPCODE_1 << 24)) |
+					        (((*addr & 0x00FFFFFF) - ENC_VAL_2) & 0x00FFFFFF);
 					
-					xorval ^= *(u32*)addr >> 24;
+					xorval ^= *addr >> 24;
 					xorval &= 0x00FFFFFF;
 					break;
 				
 				case INS_TYPE_BL:
 					// Link bit
-					*(u32*)addr ^= (ENC_OPCODE_1 << 24);
+					*addr ^= (ENC_OPCODE_1 << 24);
 					// Fall through
 				default:
-					*(u32*)addr ^= xorval;
+					*addr ^= xorval;
 					
-					xorval ^= *(u32*)addr;
-					xorval ^= *(u32*)addr >> 8;
+					xorval ^= *addr;
+					xorval ^= *addr >> 8;
 					xorval &= 0x00FFFFFF;
 					break;
 			}
