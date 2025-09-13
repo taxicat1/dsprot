7915339b89145c7af52fd0831b80e11ebf6a99e9
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-13 19:34:50 -0400

    Improve RC4 decomp

--- src/rc4.c
+++ src/rc4.c
@@ -1,5 +1,7 @@
 #include "rc4.h"
 
+#define RC4_KEY_SIZE  (16)
+
 
 void RC4_Init(RC4_Ctx* ctx, const void* key, u32 key_len) {
 	u8   tmp1;
@@ -35,29 +37,23 @@ void RC4_Init(RC4_Ctx* ctx, const void* key, u32 key_len) {
 
 
 u8 RC4_Byte(RC4_Ctx* ctx) {
-	u8   i;
-	u8*  S;
-	u8   jval;
-	u8   ival;
-	u8   j;
-	u8   out_idx;
+	u8  i;
+	u8  ival;
+	u8  j;
+	u8  jval;
 	
 	i = ctx->i + 1;
-	
-	S = ctx->S;
-	
-	ival = S[i];
+	ival = ctx->S[i];
 	j = ival + ctx->j;
-	jval = S[j];
+	jval = ctx->S[j];
 	
 	ctx->i = i;
 	ctx->j = j;
 	
-	S[j] = ival;
-	S[i] = jval;
+	ctx->S[j] = ival;
+	ctx->S[i] = jval;
 	
-	out_idx = ival + jval;
-	return ctx->S[out_idx];
+	return ctx->S[(ival + jval) & 0xFF];
 }
 
 
@@ -107,7 +103,7 @@ u32 RC4_DecryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 
 u32 RC4_InitAndEncryptInstructions(void* key, void* dst, void* src, u32 size) {
 	RC4_Ctx ctx;
-	RC4_Init(&ctx, key, 16);
+	RC4_Init(&ctx, key, RC4_KEY_SIZE);
 	// Must coerce output to -1 or 0 like this to match
 	return RC4_EncryptInstructions(&ctx, dst, src, size) == -1 ? -1 : 0;
 }
@@ -115,7 +111,7 @@ u32 RC4_InitAndEncryptInstructions(void* key, void* dst, void* src, u32 size) {
 
 u32 RC4_InitAndDecryptInstructions(void* key, void* dst, void* src, u32 size) {
 	RC4_Ctx ctx;
-	RC4_Init(&ctx, key, 16);
+	RC4_Init(&ctx, key, RC4_KEY_SIZE);
 	// Must coerce output to -1 or 0 like this to match
 	return RC4_DecryptInstructions(&ctx, dst, src, size) == -1 ? -1 : 0;
 }
