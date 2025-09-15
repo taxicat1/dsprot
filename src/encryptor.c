276cd4f3e515adfa741c56350df1eb173813bad1
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-15 17:25:47 -0400

    0 -> NULL

--- src/encryptor.c
+++ src/encryptor.c
@@ -102,7 +102,7 @@ void Encryptor_DecodeFunctionTable(FuncInfo* functions) {
 		functions->start_addr = NULL;
 		functions++;
 		
-	} while (functions->start_addr != 0);
+	} while (functions->start_addr != NULL);
 }
 
 
