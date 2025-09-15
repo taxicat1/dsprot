cd6c633135efa31ffbd6dfe0257608925362de1c
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-15 08:57:10 -0400

    Improve encryptor decomp

--- src/encryptor.c
+++ src/encryptor.c
@@ -8,11 +8,12 @@ void Encryptor_StartRange(u32* addr) {
 	int  i;
 	u32  key_ins;
 	u8*  keyptr;
-	u32  end;
 	u32  size;
 	
+	// First key is immediately prior to start address
 	key_ins = addr[-1];
 	
+	// Derive RC4 key
 	keyptr = &key[0];
 	for (i = 0; i < 16; i++) {
 		*keyptr = key_ins >> ((i % 4) * 8);
@@ -22,12 +23,11 @@ void Encryptor_StartRange(u32* addr) {
 		keyptr++;
 	}
 	
-	end = addr[0];
-	size = 0;
-	while (key_ins != end) {
-		size++;
-		end = addr[size];
-	}
+	// Search forward for second key to determine size
+    size = 0;
+    while (key_ins != addr[size]) {
+        size++;
+    }
 	
 	if (size) {
 		RC4_InitAndDecryptInstructions(&key[0], addr, addr, size * 4);
@@ -43,11 +43,12 @@ void Encryptor_EndRange(u32* addr) {
 	int  i;
 	u32  key_ins;
 	u8*  keyptr;
-	u32  end;
 	u32  size;
 	
+	// Second key is immediately following the end address
 	key_ins = addr[1];
 	
+	// Derive RC4 key
 	keyptr = &key[0];
 	for (i = 0; i < 16; i++) {
 		*keyptr = key_ins >> ((i % 4) * 8);
@@ -57,18 +58,19 @@ void Encryptor_EndRange(u32* addr) {
 		keyptr++;
 	}
 	
-	end = addr[0];
-	while (end != key_ins) {
-		end = *--addr;
+	// Search backward for first key
+	while (*addr != key_ins) {
+		addr--;
 	}
-	end = addr[1];
 	
-	size = 0;
-	while (key_ins != end) {
-		size++;
-		end = addr[size + 1];
-	}
+	// The start address comes immediately after the first key
 	addr++;
+	
+	// Search forward from start to determine size
+	size = 0;
+	while (key_ins != addr[size]) {
+		size++;
+	}
 	
 	if (size) {
 		RC4_InitAndEncryptInstructions(&key[0], addr, addr, size * 4);
