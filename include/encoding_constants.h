796d40893b826b815ab36f0ae34f230ec9f79f43
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-15 22:51:54 -0400

    Address obfuscation to macro

--- include/encoding_constants.h
+++ include/encoding_constants.h
@@ -6,6 +6,8 @@
 
 #define ENC_OPCODE_1  (0x01)
 
+#define ADDR_PLUS_ADDEND(ref, addend)  ((u32)(&ref + ((addend) / sizeof(*&ref))))
+
 #define ENC_XOR_START  (0xF03852CB)
 
 #define ENC_RC4_X_START  (0xAA)
