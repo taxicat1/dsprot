e1f7f53b3d9c334fdf851fa003af8049965e911a
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-15 22:41:34 -0400

    Address obfuscation to macro

--- src/integrity.c
+++ src/integrity.c
@@ -25,7 +25,7 @@ u32 Integrity_MACOwner_IsBad(void) {
 	u32  offset;
 	
 	// Obfuscated handling of function address
-	addr = (u8*)(&RunEncrypted_MACOwner_IsBad + ENC_VAL_1) - (ENC_VAL_1 * 2);
+	addr = (u8*)ADDR_PLUS_ADDEND(RunEncrypted_MACOwner_IsBad, ENC_VAL_1) - (ENC_VAL_1 * 2);
 	ret = (u32)addr + 1;
 	addr += INTEGRITY_OBFS_OFFSET;
 	offset = ENC_VAL_1 - INTEGRITY_OBFS_OFFSET;
@@ -61,7 +61,7 @@ u32 Integrity_MACOwner_IsGood(void) {
 	u32  offset;
 	
 	// Obfuscated handling of function address
-	addr = (u8*)(&RunEncrypted_MACOwner_IsGood + ENC_VAL_1) - (ENC_VAL_1 * 2);
+	addr = (u8*)ADDR_PLUS_ADDEND(RunEncrypted_MACOwner_IsGood, ENC_VAL_1) - (ENC_VAL_1 * 2);
 	ret = (u32)addr;
 	addr += INTEGRITY_OBFS_OFFSET;
 	offset = ENC_VAL_1 - INTEGRITY_OBFS_OFFSET;
@@ -97,7 +97,7 @@ u32 Integrity_ROMTest_IsBad(void) {
 	u32  offset;
 	
 	// Obfuscated handling of function address
-	addr = (u8*)(&RunEncrypted_ROMTest_IsBad + ENC_VAL_1) - (ENC_VAL_1 * 2);
+	addr = (u8*)ADDR_PLUS_ADDEND(RunEncrypted_ROMTest_IsBad, ENC_VAL_1) - (ENC_VAL_1 * 2);
 	ret = (u32)addr + 1;
 	addr += INTEGRITY_OBFS_OFFSET;
 	offset = ENC_VAL_1 - INTEGRITY_OBFS_OFFSET;
@@ -133,7 +133,7 @@ u32 Integrity_ROMTest_IsGood(void) {
 	u32  offset;
 	
 	// Obfuscated handling of function address
-	addr = (u8*)(&RunEncrypted_ROMTest_IsGood + ENC_VAL_1) - (ENC_VAL_1 * 2);
+	addr = (u8*)ADDR_PLUS_ADDEND(RunEncrypted_ROMTest_IsGood, ENC_VAL_1) - (ENC_VAL_1 * 2);
 	ret = (u32)addr;
 	addr += INTEGRITY_OBFS_OFFSET;
 	offset = ENC_VAL_1 - INTEGRITY_OBFS_OFFSET;
