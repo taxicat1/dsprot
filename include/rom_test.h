6e06f620d2d5bbe99ae0cfef3e92d4af0e055a96
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-18 18:19:16 -0400

    Whitespace

--- include/rom_test.h
+++ include/rom_test.h
@@ -14,52 +14,52 @@ extern const u8* CARD_GetRomHeader(void);
 
 // Copy of TWL structs
 typedef struct {
-    u32  offset;
-    u32  length;
+	u32  offset;
+	u32  length;
 } CARDRomRegion;
 
 typedef struct {
-    char    game_name[12];             /* Software title name*/
-    u32     game_code;                 /* initial code*/
-    u16     maker_code;                /* maker code*/
-    u8      product_id;                /* system code*/
-    u8      device_type;               /* Device type*/
-    u8      device_size;               /* device capacity*/
-    u8      reserved_A[9];             /* system reserve A*/
-    u8      game_version;              /* software version*/
-    u8      property;                  /* internal use flag*/
+	char    game_name[12];             /* Software title name*/
+	u32     game_code;                 /* initial code*/
+	u16     maker_code;                /* maker code*/
+	u8      product_id;                /* system code*/
+	u8      device_type;               /* Device type*/
+	u8      device_size;               /* device capacity*/
+	u8      reserved_A[9];             /* system reserve A*/
+	u8      game_version;              /* software version*/
+	u8      property;                  /* internal use flag*/
 	
-    void   *main_rom_offset;           /* ARM9 transfer source ROM offset*/
-    void   *main_entry_address;        /* ARM9 execution start address (un-mounted)*/
-    void   *main_ram_address;          /* ARM9 transfer destination RAM offset*/
-    u32     main_size;                 /* ARM9 distribution size*/
-    void   *sub_rom_offset;            /* ARM7 transfer source ROM offset*/
-    void   *sub_entry_address;         /* ARM7 execution start address (un-mounted)*/
-    void   *sub_ram_address;           /* ARM7 transfer destination RAM offset*/
-    u32     sub_size;                  /* ARM7 distribution size*/
+	void   *main_rom_offset;           /* ARM9 transfer source ROM offset*/
+	void   *main_entry_address;        /* ARM9 execution start address (un-mounted)*/
+	void   *main_ram_address;          /* ARM9 transfer destination RAM offset*/
+	u32     main_size;                 /* ARM9 distribution size*/
+	void   *sub_rom_offset;            /* ARM7 transfer source ROM offset*/
+	void   *sub_entry_address;         /* ARM7 execution start address (un-mounted)*/
+	void   *sub_ram_address;           /* ARM7 transfer destination RAM offset*/
+	u32     sub_size;                  /* ARM7 distribution size*/
 	
-    CARDRomRegion fnt;                 /* File Name Table*/
-    CARDRomRegion fat;                 /* File allocation table.*/
+	CARDRomRegion  fnt;                /* File Name Table*/
+	CARDRomRegion  fat;                /* File allocation table.*/
 	
-    CARDRomRegion main_ovt;            /* ARM9 overlay header table*/
-    CARDRomRegion sub_ovt;             /* ARM7 overlay header table*/
+	CARDRomRegion  main_ovt;           /* ARM9 overlay header table*/
+	CARDRomRegion  sub_ovt;            /* ARM7 overlay header table*/
 	
-    u8      rom_param_A[8];            /* Mask ROM control parameter A*/
-    u32     banner_offset;             /* Banner file ROM offset*/
-    u16     secure_crc;                /* Secure environment CRC*/
-    u8      rom_param_B[2];            /* Mask ROM control parameter B*/
+	u8      rom_param_A[8];            /* Mask ROM control parameter A*/
+	u32     banner_offset;             /* Banner file ROM offset*/
+	u16     secure_crc;                /* Secure environment CRC*/
+	u8      rom_param_B[2];            /* Mask ROM control parameter B*/
 	
-    void   *main_autoload_done;        /* ARM9 auto load hook address*/
-    void   *sub_autoload_done;         /* ARM7 auto load hook address*/
+	void   *main_autoload_done;        /* ARM9 auto load hook address*/
+	void   *sub_autoload_done;         /* ARM7 auto load hook address*/
 	
-    u8      rom_param_C[8];            /* Mask ROM control parameter C*/
-    u32     rom_size;                  /* Application final ROM offset*/
-    u32     header_size;               /* ROM header size*/
-    u8      reserved_B[0x38];          /* System reserve B*/
+	u8      rom_param_C[8];            /* Mask ROM control parameter C*/
+	u32     rom_size;                  /* Application final ROM offset*/
+	u32     header_size;               /* ROM header size*/
+	u8      reserved_B[0x38];          /* System reserve B*/
 	
-    u8      logo_data[0x9C];           /* NINTENDO logo image data*/
-    u16     logo_crc;                  /* NINTENDO logo CRC*/
-    u16     header_crc;                /* ROM internal register data CRC*/
+	u8      logo_data[0x9C];           /* NINTENDO logo image data*/
+	u16     logo_crc;                  /* NINTENDO logo CRC*/
+	u16     header_crc;                /* ROM internal register data CRC*/
 } CARDRomHeader;
 
 // TWL function without header support (anymore)
