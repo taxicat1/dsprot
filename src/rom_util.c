796583f3ab82960ab25d3ceea93d39442ff0e369
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-19 14:26:16 -0400

    Small corrections

--- src/rom_util.c
+++ src/rom_util.c
@@ -63,7 +63,7 @@ void ROMUtil_Read(void* dest, u32 addr, s32 num_bytes) {
 	reg_MI_EXMEMCNT &= ~REG_MI_EXMEMCNT_MP_MASK;
 	
 	// Obfuscated, create address 0x027FFE60
-	// This is an address in the .nds header: port 0x040001A4 / setting for normal commands
+	// This is an address in the ROM header: port 0x040001A4 / setting for normal commands
 	card_ctrl_13 = 5;
 	card_ctrl_13 <<= 18;
 	card_ctrl_13 -= 13;
