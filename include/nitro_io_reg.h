4154e23438111c97b387d7ddb6872710f1534dc8
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-19 14:25:06 -0400

    Whitespace

--- include/nitro_io_reg.h
+++ include/nitro_io_reg.h
@@ -4,8 +4,7 @@
 #include "nitro_types.h"
 
 // <nitro/hw/io_reg.h>
-#define HW_REG_BASE  (0x04000000)
-
+#define HW_REG_BASE              (0x04000000)
 #define REG_EXMEMCNT_OFFSET      (0x204)
 #define reg_MI_EXMEMCNT          (*(REGType16v*)(HW_REG_BASE + REG_EXMEMCNT_OFFSET))
 #define REG_MI_EXMEMCNT_MP_MASK  (0x800)
