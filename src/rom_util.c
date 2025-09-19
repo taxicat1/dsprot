dca41256a3c8970e67e40abaa40daf2749b689e8
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-19 16:35:15 -0400

    Whitespace

--- src/rom_util.c
+++ src/rom_util.c
@@ -22,7 +22,6 @@
 
 #define MROMOP_G_READ_PAGE  (0xB7000000)
 
-
 // Functions to be encrypted (cannot be called directly)
 void ROMUtil_Read(void* dest, u32 addr, s32 num_bytes);
 u32 ROMUtil_CRC32(void* buf, u32 size);
