cffbc0bab2338ae963d877ee400b60362731f578
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-20 01:30:51 -0400

    Document DSi mode detection

--- src/rom_test.c
+++ src/rom_test.c
@@ -111,8 +111,8 @@ u32 ROMTest_IsBad(DSProt_Ctx* ctx) {
 			addr_offset = addr & addr_mask;
 			
 			// Creating address 0x027FFE60 cont.
-			// This read is not a used location, should always read 0
-			card_ctrl_13 += *(REGType8v*)(register_base_1 + 0x4000) & 1;
+			// If the system is in DSi mode, the address is changed to 0x02FFFE60
+			card_ctrl_13 += *(REGType8v*)(register_base_1 + REG_A9ROM_OFFSET) & REG_SCFG_A9ROM_SEC_MASK;
 			card_ctrl_13 <<= 18;
 			card_ctrl_13 -= 13;
 			card_ctrl_13 <<= 5;
@@ -259,7 +259,7 @@ u32 ROMTest_IsBad(DSProt_Ctx* ctx) {
 			addr_mask = (CARD_ROM_PAGE_SIZE + 4) - card_ctrl_13;
 			addr_offset = addr & addr_mask;
 			
-			card_ctrl_13 += *(REGType8v*)(register_base_1 + 0x4000) & 1;
+			card_ctrl_13 += *(REGType8v*)(register_base_1 + REG_A9ROM_OFFSET) & REG_SCFG_A9ROM_SEC_MASK;
 			card_ctrl_13 <<= 18;
 			card_ctrl_13 -= 13;
 			card_ctrl_13 <<= 5;
@@ -458,8 +458,8 @@ u32 ROMTest_IsGood(DSProt_Ctx* ctx) {
 			addr_offset = addr & addr_mask;
 			
 			// Creating address 0x027FFE60 cont.
-			// This read is not a used location, should always read 0
-			card_ctrl_13 += *(REGType8v*)(register_base_1 + 0x4000) & 1;
+			// If the system is in DSi mode, the address is changed to 0x02FFFE60
+			card_ctrl_13 += *(REGType8v*)(register_base_1 + REG_A9ROM_OFFSET) & REG_SCFG_A9ROM_SEC_MASK;
 			card_ctrl_13 <<= 18;
 			card_ctrl_13 -= 13;
 			card_ctrl_13 <<= 5;
@@ -606,7 +606,7 @@ u32 ROMTest_IsGood(DSProt_Ctx* ctx) {
 			addr_mask = (CARD_ROM_PAGE_SIZE + 4) - card_ctrl_13;
 			addr_offset = addr & addr_mask;
 			
-			card_ctrl_13 += *(REGType8v*)(register_base_1 + 0x4000) & 1;
+			card_ctrl_13 += *(REGType8v*)(register_base_1 + REG_A9ROM_OFFSET) & REG_SCFG_A9ROM_SEC_MASK;
 			card_ctrl_13 <<= 18;
 			card_ctrl_13 -= 13;
 			card_ctrl_13 <<= 5;
