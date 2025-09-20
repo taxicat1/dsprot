59658eb22e2bf3669fec6cf91054a6195c4ac53f
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-20 01:28:36 -0400

    Document DSi mode detection

--- src/rom_test.c
+++ src/rom_test.c
@@ -104,8 +104,8 @@ u32 ROMTest_IsBad(void* __unused) {
 			addr_offset = addr & addr_mask;
 			
 			// Creating address 0x027FFE60 cont.
-			// This read is not a used location, should always read 0
-			card_ctrl_13 += *(REGType8v*)(register_base_1 + 0x4000) & 1;
+			// If the system is in DSi mode, the address is changed to 0x02FFFE60
+			card_ctrl_13 += *(REGType8v*)(register_base_1 + REG_A9ROM_OFFSET) & REG_SCFG_A9ROM_SEC_MASK;
 			card_ctrl_13 <<= 18;
 			card_ctrl_13 -= 13;
 			card_ctrl_13 <<= 5;
