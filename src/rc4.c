ff6066772d89f0fba6082f200fb8c5b93c71063b
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-21 01:16:00 -0400

    Improve RC4_InitSBox

--- src/rc4.c
+++ src/rc4.c
@@ -70,11 +70,9 @@ static u8 RC4_Byte(RC4_Ctx* ctx) {
 
 
 static u32 RC4_InitSBox(u8* sbox) {
-	int  i;
-	int  x;
+	int i;
 	for (i = 0; i < 256; i++) {
-		x = i & 0xFF;
-		sbox[i] = x ^ 1;
+		sbox[i] = (u8)i ^ 1;
 	}
 	
 	return 0;
