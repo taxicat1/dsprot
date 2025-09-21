c21e1208149c7925e86ad2cb08bae27ba238b879
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-21 18:44:08 -0400

    Fix include order

--- src/crash.c
+++ src/crash.c
@@ -1,8 +1,8 @@
 #include "crash.h"
 
 #include "encoding_constants.h"
-#include "nitro_os.h"
 #include "nitro_mi.h"
+#include "nitro_os.h"
 
 // Function to be encrypted (cannot be called directly)
 void* Crash(register void* __unused1, register void* __unused2);
