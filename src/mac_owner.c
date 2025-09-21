b61aaf40199fcacb565a93a19f3b443cfedc7c75
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-21 17:53:41 -0400

    Whitespace consistency

--- src/mac_owner.c
+++ src/mac_owner.c
@@ -39,9 +39,9 @@ u32 MACOwner_IsBad(void* __unused) {
 	
 	OS_GetOwnerInfo(&owner_info);
 	if (
-		i == MAC_ADDR_SIZE &&
-		owner_info.birthday.month == 1 &&
-		owner_info.birthday.day   == 1 &&
+		i == MAC_ADDR_SIZE && 
+		owner_info.birthday.month == 1 && 
+		owner_info.birthday.day   == 1 && 
 		owner_info.nickNameLength == 0
 	) {
 		DSProt_Crash(NULL, NULL);
