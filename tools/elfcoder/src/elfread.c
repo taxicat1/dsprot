c4bd16261bcf4a41abebb458c9cd445df981ae2b
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-21 17:47:12 -0400

    Whitespace consistency

--- tools/elfcoder/src/elfread.c
+++ tools/elfcoder/src/elfread.c
@@ -193,7 +193,7 @@ static void encodeRelocations(
 		fread(&reloc, sizeof(Elf32_Rela), 1, elf->fhandle);
 		
 		if (
-			(reloc.r_offset >= symbol->st_value) &&
+			(reloc.r_offset >= symbol->st_value) && 
 			(reloc.r_offset < (symbol->st_value + encoded_size))
 		) {
 			if (task->encoding_type == ENC_DECODE) {
