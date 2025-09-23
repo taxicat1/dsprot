b7fddff5a2660235fbb098f1bf9ad2b4abc42ab4
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-22 22:17:56 -0400

    Clarify when a function may be static

--- src/encryptor.c
+++ src/encryptor.c
@@ -7,6 +7,10 @@
 
 #define ROTL(x, a)  ((a) == 0 ? (x) : (((x) << (a)) | ((x) >> (32 - (a)))))
 
+// Functions to be encoded (cannot be static)
+void* Encryptor_DecryptFunction(u32 obfs_key, u32 obfs_func_addr, u32 obfs_size);
+u32 Encryptor_EncryptFunction(u32 obfs_key, u32 obfs_func_addr, u32 obfs_size);
+
 static void clearDataAndInstructionCache(register void* start_addr, register u32 num_bytes);
 
 
