1036a8e34ca0c8e94f5bde329805c640a9b123aa
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-23 21:55:17 -0400

    Use Nitro inline for CARD_ReadRom

--- src/rom_test.c
+++ src/rom_test.c
@@ -191,9 +191,9 @@ u32 ROMTest_IsBad(void* __unused) {
 		
 		crcs[i] = RunEncrypted_ROMUtil_CRC32(&rom_buf[0], ROM_BLOCK_SIZE);
 		
-		// For above 8000h reads, use the SDK `CARDi_ReadRom`
+		// For above 8000h reads, use the SDK `CARD_ReadRom`
 		// This function is patched over on flashcarts, which can be detected
-		CARDi_ReadRom(MI_DMA_NOT_USE, (void*)(rom_addr + 0x7000), &rom_buf[0], ROM_BLOCK_SIZE, NULL, NULL, FALSE);
+		CARD_ReadRom(MI_DMA_NOT_USE, (void*)(rom_addr + 0x7000), &rom_buf[0], ROM_BLOCK_SIZE);
 		
 		// Second CRC integrity check
 		{
