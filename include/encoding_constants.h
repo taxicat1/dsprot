86d03981564997c423a073d6a6138163c5a0f6a6
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-27 05:43:27 -0400

    More magic numbers to #defines

--- include/encoding_constants.h
+++ include/encoding_constants.h
@@ -1,12 +1,15 @@
 #ifndef ENCODING_CONSTANTS_H
 #define ENCODING_CONSTANTS_H
 
+#define INS_OPCODE_LINKBIT  (0x01)
+#define INS_OPCODE_MASK     (0xFF000000)
+#define INS_OPCODE_SHIFT    (24)
+#define INS_OPERANDS_MASK   (0x00FFFFFF)
+
+#define ADDR_PLUS_ADDEND(ref, addend)  ((u32)(&ref + ((addend) / sizeof(ref))))
+
 #define ENC_VAL_1  (0x1700)
 #define ENC_VAL_2  ((ENC_VAL_1 >> 2) + 2)
-
-#define ENC_OPCODE_1  (0x01)
-
-#define ADDR_PLUS_ADDEND(ref, addend)  ((u32)(&ref + ((addend) / sizeof(*&ref))))
 
 #define ENC_XOR_START  (0xA5CA49B3)
 
