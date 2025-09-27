e85bee3c29ad8b65e36f8e49eaaf4a0233dccaf1
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-27 05:38:55 -0400

    More magic numbers to #defines

--- src/rc4.c
+++ src/rc4.c
@@ -57,16 +57,19 @@ enum {
 
 
 u32 RC4_CategorizeInstruction(u32 instruction) {
-	u8 upper_byte;
+	u8 opcode;
 	
-	upper_byte = (instruction >> 24) & 0xFF;
+	opcode = instruction >> INS_OPCODE_SHIFT;
 	
-	if ((upper_byte & 0x0E) == 0x0A) {
-		if ((upper_byte & 0xF0) == 0xF0) {
+	// Branch instruction
+	if ((opcode & 0x0E) == 0x0A) {
+		// BLX immediate type
+		if ((opcode & 0xF0) == 0xF0) {
 			return INS_TYPE_BLXIMM;
 		}
 		
-		if (upper_byte & 0x01) {
+		// Link bit
+		if (opcode & INS_OPCODE_LINKBIT) {
 			return INS_TYPE_BL;
 		} else {
 			return INS_TYPE_B;
@@ -81,7 +84,7 @@ void RC4_Init(RC4_Ctx* ctx, const void* key, u32 key_len) {
 	u8    tmp1;
 	u8    tmp2;
 	int   i;
-	int   Si;
+	u8    Si;
 	int   Ki;
 	u32*  s_start;
 	u32*  s_end;
@@ -108,7 +111,7 @@ void RC4_Init(RC4_Ctx* ctx, const void* key, u32 key_len) {
 	// Modification to RC4: i = 255 -> 0, instead of 0 -> 255
 	for (i = 255; i >= 0; i--) {
 		tmp1 = ctx->S[i];
-		Si = (Si + ((u8*)key)[Ki] + tmp1) & 0xFF;
+		Si = Si + ((u8*)key)[Ki] + tmp1;
 		tmp2 = ctx->S[Si];
 		
 		ctx->S[Si] = tmp1;
@@ -167,27 +170,28 @@ u32 RC4_EncryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 		
 		switch (((FuncType_RC4_CategorizeInstruction)(Proxy_RC4_CategorizeInstruction - ENC_VAL_1))(ins_word)) {
 			case INS_TYPE_BLXIMM:
-				// Error correction (this case should never be run)
-				src_bytes[idx+3] ^= ENC_OPCODE_1;
+				// Link bit
+				src_bytes[idx+3] ^= INS_OPCODE_LINKBIT;
 				// Fall through
 			case INS_TYPE_BL:
 				{
-					u32 upper, lower;
+					u32  opcode;
+					u32  operands;
 					u32* src_addr = (u32*)(src_bytes + idx);
 					u32* dst_addr = (u32*)(dst_bytes + idx);
 					
 					*dst_addr = *src_addr;
 					
-					upper = ((*dst_addr & 0xFF000000) ^ (ENC_OPCODE_1 << 24));
-					lower = (((*dst_addr & 0x00FFFFFF) + ENC_VAL_2) & 0x00FFFFFF);
+					opcode = ((*dst_addr & INS_OPCODE_MASK) ^ (INS_OPCODE_LINKBIT << INS_OPCODE_SHIFT));
+					operands = (((*dst_addr & INS_OPERANDS_MASK) + ENC_VAL_2) & INS_OPERANDS_MASK);
 					
-					*dst_addr = upper | lower;
+					*dst_addr = opcode | operands;
 				}
 				break;
 			
 			case INS_TYPE_B:
 				// Link bit
-				*(u32*)(src_bytes + idx) ^= (ENC_OPCODE_1 << 24);
+				*(u32*)(src_bytes + idx) ^= (INS_OPCODE_LINKBIT << INS_OPCODE_SHIFT);
 				// Fall through
 			default:
 				rc4_byte_addr = Proxy_RC4_Byte;
@@ -265,14 +269,19 @@ u32 RC4_DecryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 		
 		switch (((FuncType_RC4_CategorizeInstruction)(Proxy_RC4_CategorizeInstruction - ENC_VAL_1))(ins_word)) {
 			case INS_TYPE_BLXIMM:
-				// Error correction (this case should never run)
-				src_bytes[idx+3] ^= ENC_OPCODE_1;
+				// Link bit
+				src_bytes[idx+3] ^= INS_OPCODE_LINKBIT;
 				// Fall through
 			case INS_TYPE_B:
 				{
+					u32  opcode;
+					u32  operands;
 					u32* dst_addr = (u32*)(dst_bytes + idx);
-					*dst_addr = ((ins_word & 0xFF000000) ^ (ENC_OPCODE_1 << 24)) |
-					            (((ins_word & 0x00FFFFFF) - ENC_VAL_2) & 0x00FFFFFF);
+					
+					opcode = (ins_word & INS_OPCODE_MASK) ^ (INS_OPCODE_LINKBIT << INS_OPCODE_SHIFT);
+					operands = ((ins_word & INS_OPERANDS_MASK) - ENC_VAL_2) & INS_OPERANDS_MASK;
+					
+					*dst_addr = opcode | operands;
 				}
 				break;
 			
@@ -306,7 +315,7 @@ u32 RC4_DecryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 				}
 				
 				// Fourth byte + link bit
-				dst_bytes[idx+3] = src_bytes[idx+3] ^ ENC_OPCODE_1;
+				dst_bytes[idx+3] = src_bytes[idx+3] ^ INS_OPCODE_LINKBIT;
 				break;
 			
 			default:
