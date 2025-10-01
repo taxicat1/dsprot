cae28c4422cbacd3bdb8e359c323287acf114b64
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-01 18:05:42 -0400

    Improve clarity, magic number to #define

--- src/dsprot_main.c
+++ src/dsprot_main.c
@@ -16,6 +16,8 @@ u32 __DSProt_DetectNotDummy(u32 callback_addr);
 
 #define DSP_OBFS_OFFSET  (0x320)
 
+#define FUNC_QUEUE_END  (0)
+
 typedef u32 (*TaskFunc)(void);
 typedef void (*CallbackFunc)(void);
 
@@ -25,15 +27,15 @@ u32 __DSProt_DetectFlashcart(u32 callback_addr) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	BOOL  result;
+	BOOL  ret;
 	
-	result = FALSE;
+	ret = FALSE;
 	
 	func_queue[0] = (u32)&ROMTest_IsBad + DSP_OBFS_OFFSET;
-	func_queue[1] = 0;
+	func_queue[1] = FUNC_QUEUE_END;
 	
 	func_result_sum = 0;
-	for (i = 0; func_queue[i] != 0; i++) {
+	for (i = 0; func_queue[i] != FUNC_QUEUE_END; i++) {
 		func_queue[i] -= DSP_OBFS_OFFSET;
 		
 		func_result = ((TaskFunc)(func_queue[i]))() != 0;
@@ -46,19 +48,19 @@ u32 __DSProt_DetectFlashcart(u32 callback_addr) {
 	callback_addr ^= DSP_OBFS_OFFSET;
 	
 	func_result_sum >>= 1;
-	if (func_result_sum) {
-		result = TRUE;
+	if (func_result_sum != 0) {
+		ret = TRUE;
 	} else if (func_result_sum == 0) {
-		result = FALSE;
+		ret = FALSE;
 	}
 	
-	if (callback_addr != 0 && result) {
+	if (callback_addr != 0 && ret) {
 		((CallbackFunc)callback_addr)();
 	}
 	
 	ENCRYPTION_END(KEY_DSPROT_MAIN_1);
 	
-	return (u32)result;
+	return (u32)ret;
 }
 
 
@@ -67,15 +69,15 @@ u32 __DSProt_DetectNotFlashcart(u32 callback_addr) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	BOOL  result;
+	BOOL  ret;
 	
-	result = FALSE;
+	ret = FALSE;
 	
 	func_queue[0] = (u32)&ROMTest_IsBad + DSP_OBFS_OFFSET;
-	func_queue[1] = 0;
+	func_queue[1] = FUNC_QUEUE_END;
 	
 	func_result_sum = 0;
-	for (i = 0; func_queue[i] != 0; i++) {
+	for (i = 0; func_queue[i] != FUNC_QUEUE_END; i++) {
 		func_queue[i] -= DSP_OBFS_OFFSET;
 		
 		func_result = ((TaskFunc)(func_queue[i]))() != 0;
@@ -88,19 +90,19 @@ u32 __DSProt_DetectNotFlashcart(u32 callback_addr) {
 	callback_addr ^= DSP_OBFS_OFFSET;
 	
 	func_result_sum >>= 1;
-	if (func_result_sum) {
-		result = FALSE;
+	if (func_result_sum != 0) {
+		ret = FALSE;
 	} else if (func_result_sum == 0) {
-		result = TRUE;
+		ret = TRUE;
 	}
 	
-	if (callback_addr != 0 && result) {
+	if (callback_addr != 0 && ret) {
 		((CallbackFunc)callback_addr)();
 	}
 	
 	ENCRYPTION_END(KEY_DSPROT_MAIN_2);
 	
-	return (u32)result;
+	return (u32)ret;
 }
 
 
@@ -109,15 +111,15 @@ u32 __DSProt_DetectEmulator(u32 callback_addr) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	BOOL  result;
+	BOOL  ret;
 	
-	result = FALSE;
+	ret = FALSE;
 	
 	func_queue[0] = (u32)&MACOwner_IsBad + DSP_OBFS_OFFSET;
-	func_queue[1] = 0;
+	func_queue[1] = FUNC_QUEUE_END;
 	
 	func_result_sum = 0;
-	for (i = 0; func_queue[i] != 0; i++) {
+	for (i = 0; func_queue[i] != FUNC_QUEUE_END; i++) {
 		func_queue[i] -= DSP_OBFS_OFFSET;
 		
 		func_result = ((TaskFunc)(func_queue[i]))() != 0;
@@ -130,19 +132,19 @@ u32 __DSProt_DetectEmulator(u32 callback_addr) {
 	callback_addr ^= DSP_OBFS_OFFSET;
 	
 	func_result_sum >>= 1;
-	if (func_result_sum) {
-		result = TRUE;
+	if (func_result_sum != 0) {
+		ret = TRUE;
 	} else if (func_result_sum == 0) {
-		result = FALSE;
+		ret = FALSE;
 	}
 	
-	if (callback_addr != 0 && result) {
+	if (callback_addr != 0 && ret) {
 		((CallbackFunc)callback_addr)();
 	}
 	
 	ENCRYPTION_END(KEY_DSPROT_MAIN_3);
 	
-	return (u32)result;
+	return (u32)ret;
 }
 
 
@@ -151,15 +153,15 @@ u32 __DSProt_DetectNotEmulator(u32 callback_addr) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	BOOL  result;
+	BOOL  ret;
 	
-	result = FALSE;
+	ret = FALSE;
 	
 	func_queue[0] = (u32)&MACOwner_IsBad + DSP_OBFS_OFFSET;
-	func_queue[1] = 0;
+	func_queue[1] = FUNC_QUEUE_END;
 	
 	func_result_sum = 0;
-	for (i = 0; func_queue[i] != 0; i++) {
+	for (i = 0; func_queue[i] != FUNC_QUEUE_END; i++) {
 		func_queue[i] -= DSP_OBFS_OFFSET;
 		
 		func_result = ((TaskFunc)(func_queue[i]))() != 0;
@@ -172,19 +174,19 @@ u32 __DSProt_DetectNotEmulator(u32 callback_addr) {
 	callback_addr ^= DSP_OBFS_OFFSET;
 	
 	func_result_sum >>= 1;
-	if (func_result_sum) {
-		result = FALSE;
+	if (func_result_sum != 0) {
+		ret = FALSE;
 	} else if (func_result_sum == 0) {
-		result = TRUE;
+		ret = TRUE;
 	}
 	
-	if (callback_addr != 0 && result) {
+	if (callback_addr != 0 && ret) {
 		((CallbackFunc)callback_addr)();
 	}
 	
 	ENCRYPTION_END(KEY_DSPROT_MAIN_4);
 	
-	return (u32)result;
+	return (u32)ret;
 }
 
 
@@ -193,15 +195,15 @@ u32 __DSProt_DetectDummy(u32 callback_addr) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	BOOL  result;
+	BOOL  ret;
 	
-	result = FALSE;
+	ret = FALSE;
 	
 	// Not optimized out here due to the asm inlines produced by the encryption macros
-	func_queue[0] = 0;
+	func_queue[0] = FUNC_QUEUE_END;
 	
 	func_result_sum = 0;
-	for (i = 0; func_queue[i] != 0; i++) {
+	for (i = 0; func_queue[i] != FUNC_QUEUE_END; i++) {
 		func_queue[i] -= DSP_OBFS_OFFSET;
 		
 		func_result = ((TaskFunc)(func_queue[i]))() != 0;
@@ -214,19 +216,19 @@ u32 __DSProt_DetectDummy(u32 callback_addr) {
 	callback_addr ^= DSP_OBFS_OFFSET;
 	
 	func_result_sum >>= 1;
-	if (func_result_sum) {
-		result = TRUE;
+	if (func_result_sum != 0) {
+		ret = TRUE;
 	} else if (func_result_sum == 0) {
-		result = FALSE;
+		ret = FALSE;
 	}
 	
-	if (callback_addr != 0 && result) {
+	if (callback_addr != 0 && ret) {
 		((CallbackFunc)callback_addr)();
 	}
 	
 	ENCRYPTION_END(KEY_DSPROT_MAIN_5);
 	
-	return (u32)result;
+	return (u32)ret;
 }
 
 
@@ -235,15 +237,15 @@ u32 __DSProt_DetectNotDummy(u32 callback_addr) {
 	BOOL  func_result;
 	s32   func_result_sum;
 	u32   i;
-	BOOL  result;
+	BOOL  ret;
 	
-	result = FALSE;
+	ret = FALSE;
 	
 	// Not optimized out here due to the asm inlines produced by the encryption macros
-	func_queue[0] = 0;
+	func_queue[0] = FUNC_QUEUE_END;
 	
 	func_result_sum = 0;
-	for (i = 0; func_queue[i] != 0; i++) {
+	for (i = 0; func_queue[i] != FUNC_QUEUE_END; i++) {
 		func_queue[i] -= DSP_OBFS_OFFSET;
 		
 		func_result = ((TaskFunc)(func_queue[i]))() != 0;
@@ -256,17 +258,17 @@ u32 __DSProt_DetectNotDummy(u32 callback_addr) {
 	callback_addr ^= DSP_OBFS_OFFSET;
 	
 	func_result_sum >>= 1;
-	if (func_result_sum) {
-		result = FALSE;
+	if (func_result_sum != 0) {
+		ret = FALSE;
 	} else if (func_result_sum == 0) {
-		result = TRUE;
+		ret = TRUE;
 	}
 	
-	if (callback_addr != 0 && result) {
+	if (callback_addr != 0 && ret) {
 		((CallbackFunc)callback_addr)();
 	}
 	
 	ENCRYPTION_END(KEY_DSPROT_MAIN_6);
 	
-	return (u32)result;
+	return (u32)ret;
 }
