df40b34bcc89e679054886f390839ae57c9d5ebe
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-01 18:20:42 -0400

    Magic number to #define

--- src/dsprot_main.c
+++ src/dsprot_main.c
@@ -16,6 +16,8 @@ u32 DetectNotDummy(void* callback);
 
 #define DSP_OBFS_OFFSET  (0x320)
 
+#define FUNC_QUEUE_END  (0)
+
 enum {
 	EXPECT_FALSE,
 	EXPECT_TRUE
@@ -37,13 +39,13 @@ static inline u32 dsprotMain(u32* func_queue, int expected_result, void* callbac
 	compare_sum = 0;
 	func_result_sum = 0;
 	
-	for (i = 0; func_queue[i] != 0; i++) {
+	for (i = 0; func_queue[i] != FUNC_QUEUE_END; i++) {
 		func_result = ((TaskFunc)(func_queue[i] - ENC_VAL_1 - DSP_OBFS_OFFSET))() != 0;
 		
 		func_result_sum += func_result;
 		func_result_sum <<= 1;
 		
-		compare_sum += 1;
+		compare_sum += TRUE;
 		compare_sum <<= 1;
 	}
 	
@@ -64,7 +66,7 @@ static inline u32 dsprotMain(u32* func_queue, int expected_result, void* callbac
 u32 DetectFlashcart(void* callback) {
 	u32 func_queue[32];
 	
-	func_queue[2] = 0;
+	func_queue[2] = FUNC_QUEUE_END;
 	func_queue[0] = ADDR_PLUS_ADDEND(RunEncrypted_ROMTest_IsBad, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	func_queue[1] = ADDR_PLUS_ADDEND(RunEncrypted_Integrity_ROMTest_IsBad, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	
@@ -75,7 +77,7 @@ u32 DetectFlashcart(void* callback) {
 u32 DetectNotFlashcart(void* callback) {
 	u32 func_queue[32];
 	
-	func_queue[2] = 0;
+	func_queue[2] = FUNC_QUEUE_END;
 	func_queue[0] = ADDR_PLUS_ADDEND(RunEncrypted_ROMTest_IsGood, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	func_queue[1] = ADDR_PLUS_ADDEND(RunEncrypted_Integrity_ROMTest_IsGood, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	
@@ -86,7 +88,7 @@ u32 DetectNotFlashcart(void* callback) {
 u32 DetectEmulator(void* callback) {
 	u32 func_queue[32];
 	
-	func_queue[2] = 0;
+	func_queue[2] = FUNC_QUEUE_END;
 	func_queue[0] = ADDR_PLUS_ADDEND(RunEncrypted_MACOwner_IsBad, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	func_queue[1] = ADDR_PLUS_ADDEND(RunEncrypted_Integrity_MACOwner_IsBad, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	
@@ -97,7 +99,7 @@ u32 DetectEmulator(void* callback) {
 u32 DetectNotEmulator(void* callback) {
 	u32 func_queue[32];
 	
-	func_queue[2] = 0;
+	func_queue[2] = FUNC_QUEUE_END;
 	func_queue[0] = ADDR_PLUS_ADDEND(RunEncrypted_MACOwner_IsGood, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	func_queue[1] = ADDR_PLUS_ADDEND(RunEncrypted_Integrity_MACOwner_IsGood, ENC_VAL_1) + DSP_OBFS_OFFSET;
 	
@@ -109,7 +111,7 @@ u32 DetectDummy(void* callback) {
 	u32 func_queue[32];
 	
 	// Prevent optimization of the function queue processing
-	*(u32*)&func_queue[0] = 0;
+	*(u32*)&func_queue[0] = FUNC_QUEUE_END;
 	
 	return dsprotMain(&func_queue[0], EXPECT_FALSE, callback);
 }
@@ -119,7 +121,7 @@ u32 DetectNotDummy(void* callback) {
 	u32 func_queue[32];
 	
 	// Prevent optimization of the function queue processing
-	*(u32*)&func_queue[0] = 0;
+	*(u32*)&func_queue[0] = FUNC_QUEUE_END;
 	
 	return dsprotMain(&func_queue[0], EXPECT_TRUE, callback);
 }
