1b870c7b5b111f749b0dadbbfd2425078b595468
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-02 12:32:20 -0400

    Rename variable idx -> offset

--- src/integrity.c
+++ src/integrity.c
@@ -16,35 +16,35 @@ u32 Integrity_ROMTest_IsGood(DSProt_Ctx* __unused);
 
 // This was likely not originally an inline, but an inline is able to match here nicely
 static inline u32 checkDecryptionWrapper(u8* addr, u32 match_ret, u32 mismatch_ret) {
-	u32 idx;
+	u32 offset;
 	
 	addr += INTEGRITY_OBFS_OFFSET;
-	idx = ENC_VAL_1 - INTEGRITY_OBFS_OFFSET;
+	offset = ENC_VAL_1 - INTEGRITY_OBFS_OFFSET;
 	
 	// The bytes checked here are from the `run_encrypted_func` macro defined in asm_macro.inc:
 	//   e18fc00f    orr    ip, pc, pc
 	//   e01cc00c    ands   ip, ip, ip
 	//   03a0c000    moveq  ip, #0
 	//   128cc068    addne  ip, ip, #104
-	if (addr[idx++] != 0x0F) return mismatch_ret;
-	if (addr[idx++] != 0xC0) return mismatch_ret;
-	if (addr[idx++] != 0x8F) return mismatch_ret;
-	if (addr[idx++] != 0xE1) return mismatch_ret;
+	if (addr[offset++] != 0x0F) return mismatch_ret;
+	if (addr[offset++] != 0xC0) return mismatch_ret;
+	if (addr[offset++] != 0x8F) return mismatch_ret;
+	if (addr[offset++] != 0xE1) return mismatch_ret;
 	
-	if (addr[idx++] != 0x0C) return mismatch_ret;
-	if (addr[idx++] != 0xC0) return mismatch_ret;
-	if (addr[idx++] != 0x1C) return mismatch_ret;
-	if (addr[idx++] != 0xE0) return mismatch_ret;
+	if (addr[offset++] != 0x0C) return mismatch_ret;
+	if (addr[offset++] != 0xC0) return mismatch_ret;
+	if (addr[offset++] != 0x1C) return mismatch_ret;
+	if (addr[offset++] != 0xE0) return mismatch_ret;
 	
-	if (addr[idx++] != 0x00) return mismatch_ret;
-	if (addr[idx++] != 0xC0) return mismatch_ret;
-	if (addr[idx++] != 0xA0) return mismatch_ret;
-	if (addr[idx++] != 0x03) return mismatch_ret;
+	if (addr[offset++] != 0x00) return mismatch_ret;
+	if (addr[offset++] != 0xC0) return mismatch_ret;
+	if (addr[offset++] != 0xA0) return mismatch_ret;
+	if (addr[offset++] != 0x03) return mismatch_ret;
 	
-	if (addr[idx++] != 0x68) return mismatch_ret;
-	if (addr[idx++] != 0xC0) return mismatch_ret;
-	if (addr[idx++] != 0x8C) return mismatch_ret;
-	if (addr[idx++] != 0x12) return mismatch_ret;
+	if (addr[offset++] != 0x68) return mismatch_ret;
+	if (addr[offset++] != 0xC0) return mismatch_ret;
+	if (addr[offset++] != 0x8C) return mismatch_ret;
+	if (addr[offset++] != 0x12) return mismatch_ret;
 	
 	return match_ret;
 }
