d16495bc0e6bd5fe1b9429649e423d49e2bf8dcc
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-02 04:42:03 -0400

    Comment correction

--- src/rom_test.c
+++ src/rom_test.c
@@ -452,7 +452,7 @@ u32 ROMTest_IsBad(DSProt_Ctx* ctx) {
 	
 	// Checking the ROM reading results were as expected:
 	//   0 == 1 == 2 == 6
-	//   3 == 9 (not checked)
+	//   3 == 6 (not checked)
 	//   4 == 10
 	//   5 == 11
 	//   12 == 14
@@ -928,7 +928,7 @@ u32 ROMTest_IsGood(DSProt_Ctx* ctx) {
 	
 	// Checking the ROM reading results were as expected:
 	//   0 == 1 == 2 == 6
-	//   3 == 9 (not checked)
+	//   3 == 6 (not checked)
 	//   4 == 10
 	//   5 == 11
 	//   12 == 14
