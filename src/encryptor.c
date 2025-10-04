1cb04bd77c2a4aa7975dac319038325e5e266436
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-03 23:02:53 -0400

    Key derivation to inline

--- src/encryptor.c
+++ src/encryptor.c
@@ -113,6 +113,14 @@ void Encryptor_DecodeFunctionTable(FuncInfo* functions) {
 }
 
 
+static inline void expandRC4Key(u32 seed_key, u32 size, u32* expanded_key) {
+	expanded_key[0] = ROTL(seed_key,  0) ^ size;
+	expanded_key[1] = ROTL(seed_key,  8) ^ size;
+	expanded_key[2] = ROTL(seed_key, 16) ^ size;
+	expanded_key[3] = ROTL(seed_key, 24) ^ size;
+}
+
+
 void* Encryptor_DecryptFunction(u32 key, u32 func_addr, u32 size) {
 	u32    expanded_key[4];
 	void*  func_ptr;
@@ -125,12 +133,7 @@ void* Encryptor_DecryptFunction(u32 key, u32 func_addr, u32 size) {
 	func_ptr = (void*)func_addr;
 	func_ptr -= ENC_VAL_1;
 	
-	// Derive RC4 key
-	expanded_key[0] = ROTL(key,  0) ^ size;
-	expanded_key[1] = ROTL(key,  8) ^ size;
-	expanded_key[2] = ROTL(key, 16) ^ size;
-	expanded_key[3] = ROTL(key, 24) ^ size;
-	
+	expandRC4Key(key, size, &expanded_key[0]);
 	RC4_InitAndDecryptInstructions(&expanded_key[0], func_ptr, func_ptr, size);
 	clearDataAndInstructionCache(func_ptr, size);
 	
@@ -151,12 +154,7 @@ u32 Encryptor_EncryptFunction(u32 key, u32 func_addr, u32 size) {
 	func_ptr = (void*)func_addr;
 	func_ptr -= ENC_VAL_1;
 	
-	// Derive RC4 key
-	expanded_key[0] = ROTL(key,  0) ^ size;
-	expanded_key[1] = ROTL(key,  8) ^ size;
-	expanded_key[2] = ROTL(key, 16) ^ size;
-	expanded_key[3] = ROTL(key, 24) ^ size;
-	
+	expandRC4Key(key, size, &expanded_key[0]);
 	RC4_InitAndEncryptInstructions(&expanded_key[0], func_ptr, func_ptr, size);
 	clearDataAndInstructionCache(func_ptr, size);
 	
