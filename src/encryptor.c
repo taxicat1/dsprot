72c317f381571fce3bd8823d8964b258f27b7d37
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-04 17:00:13 -0400

    Consistent variable declaration order

--- src/encryptor.c
+++ src/encryptor.c
@@ -54,11 +54,11 @@ static inline void clearDataAndInstructionCache(void) {
 
 
 void Encryptor_DecodeFunctionTable(FuncInfo* functions) {
-	u32*  prevmem;
 	u32*  addr;
 	u32   size;
 	u32*  end_addr;
 	u32   xorval;
+	u32*  prevmem;
 	
 	// Zero memory in the function callee
 	prevmem = (u32*)functions - 3;
