fa4bb167df4bd4af5c1d7d91661232b24cc604a4
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-05 13:01:23 -0400

    More consistent RC4 steps

--- src/rc4.c
+++ src/rc4.c
@@ -209,10 +209,8 @@ u32 RC4_DecryptInstructions(RC4_Ctx* ctx, void* src, void* dst, u32 size) {
 					u32  operands;
 					u32* dst_addr = (u32*)(dst_bytes + offset);
 					
-					*dst_addr = ins_word;
-					
-					opcode = (*dst_addr & INS_OPCODE_MASK) ^ (INS_OPCODE_LINKBIT << INS_OPCODE_SHIFT);
-					operands = ((*dst_addr & INS_OPERANDS_MASK) - ENC_VAL_2) & INS_OPERANDS_MASK;
+					opcode = (ins_word & INS_OPCODE_MASK) ^ (INS_OPCODE_LINKBIT << INS_OPCODE_SHIFT);
+					operands = ((ins_word & INS_OPERANDS_MASK) - ENC_VAL_2) & INS_OPERANDS_MASK;
 					
 					*dst_addr = opcode | operands;
 				}
