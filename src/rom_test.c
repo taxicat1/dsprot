f987b8cc6ad1a589356f4720e56066af21695721
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-04 22:55:30 -0400

    Consistent variable order

--- src/rom_test.c
+++ src/rom_test.c
@@ -16,8 +16,8 @@ u32 ROMTest_IsBad(void) {
 	// Extra CRC entry is required to match
 	u32  crcs[7];
 	u8   rom_buf[ROM_BLOCK_SIZE];
-	u32  rom_addr;
 	u16  lock_id;
+	u32  rom_addr;
 	u32  mul;
 	int  i;
 	
@@ -73,8 +73,8 @@ u32 ROMTest_IsGood(void) {
 	// Extra CRC entry is required to match
 	u32  crcs[7];
 	u8   rom_buf[ROM_BLOCK_SIZE];
-	u32  rom_addr;
 	u16  lock_id;
+	u32  rom_addr;
 	u32  mul;
 	int  i;
 	
