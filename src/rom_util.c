f1eecdf662416b50c78d5a88608e583e83d75a16
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-09 22:14:19 -0400

    Consistent pointer variable naming

--- src/rom_util.c
+++ src/rom_util.c
@@ -7,13 +7,13 @@ u32 ROMUtil_CRC32(void* buf, u32 size);
 u32 ROMUtil_CRC32(void* buf, u32 size) {
 	u32  crc;
 	u32  poly;
-	u8*  byteptr;
+	u8*  byte_ptr;
 	
-	byteptr = (u8*)buf;
+	byte_ptr = (u8*)buf;
 	crc = 0xFFFFFFFF;
 	poly = 0xEDB88320;
 	while (size-- != 0) {
-		crc ^= *byteptr++;
+		crc ^= *byte_ptr++;
 		
 		// Must be unrolled to match
 		if (crc & 1) {  crc = (crc >> 1);  } else {  crc = poly ^ (crc >> 1);  }
