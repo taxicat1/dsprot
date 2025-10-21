c90c4e6b148349719f660fba4964cb7069dc243c
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-21 16:21:16 -0400

    Remove export of new API, use old as default

--- dsprot.h
+++ dsprot.h
@@ -10,14 +10,6 @@
  */
 //=================================================================================================
 
-// Expected return values if no flashcart/emulator/tampering was detected
-#define DSP_DETECTFLASHCART_OK     (1830601)
-#define DSP_DETECTNOTFLASHCART_OK  (1831551)
-#define DSP_DETECTEMULATOR_OK      (1830203)
-#define DSP_DETECTNOTEMULATOR_OK   (1830859)
-#define DSP_DETECTDUMMY_OK         (1828014)
-#define DSP_DETECTNOTDUMMY_OK      (1829648)
-
 #ifndef SDK_ASM
 
 #include <nitro/types.h>  // For u32
@@ -26,112 +18,32 @@
 extern "C" {
 #endif
 
+// Internal DS Protect functions to be called by inlines
+extern u32 __DSProt_DetectFlashcart(void* callback, void* param, u32 __unused);
+extern u32 __DSProt_DetectEmulator(void* callback, void* param, u32 __unused);
+extern u32 __DSProt_DetectDummy(void* callback, void* param, u32 __unused);
+extern u32 __DSProt_DetectNotFlashcart(void* callback, void* param, u32 __unused);
+extern u32 __DSProt_DetectNotEmulator(void* callback, void* param, u32 __unused);
+extern u32 __DSProt_DetectNotDummy(void* callback, void* param, u32 __unused);
 
-//=================================================================================================
-/**
- * Detect if the current environment is a flashcart.
- * 
- * @param callback Function to be called if the environment is determined to be a flashcart.
- *                 May be NULL.
- * @param param Parameter to be passed to the callback
- * @param __unused Unused
- * 
- * @return If the callback was run, the return of the callback. If a flashcart was not detected,
- *         DSP_DETECTFLASHCART_OK. Otherwise, some other value.
- */
-//=================================================================================================
-extern u32 DSProt_DetectFlashcart(void* callback, void* param, u32 __unused);
+// Internal wrapper functions for exporting the DS Protect API
+static u32 __DSProt_DetectFlashcart_wrapper(void* callback);
+static u32 __DSProt_DetectEmulator_wrapper(void* callback);
+static u32 __DSProt_DetectDummy_wrapper(void* callback);
+static u32 __DSProt_DetectNotFlashcart_wrapper(void* callback);
+static u32 __DSProt_DetectNotEmulator_wrapper(void* callback);
+static u32 __DSProt_DetectNotDummy_wrapper(void* callback);
 
-
-//=================================================================================================
-/**
- * Detect if the current environment is an emulator.
- * 
- * @param callback Function to be called if the environment is determined to be an emulator.
- *                 May be NULL.
- * @param param Parameter to be passed to the callback
- * @param __unused Unused
- * 
- * @return If the callback was run, the return of the callback. If an emulator was not detected,
- *         DSP_DETECTEMULATOR_OK. Otherwise, some other value.
- */
-//=================================================================================================
-extern u32 DSProt_DetectEmulator(void* callback, void* param, u32 __unused);
+// Expected return values if no flashcart/emulator/tampering was detected
+#define DSP_DETECTFLASHCART_OK     (1830601)
+#define DSP_DETECTNOTFLASHCART_OK  (1831551)
+#define DSP_DETECTEMULATOR_OK      (1830203)
+#define DSP_DETECTNOTEMULATOR_OK   (1830859)
+#define DSP_DETECTDUMMY_OK         (1828014)
+#define DSP_DETECTNOTDUMMY_OK      (1829648)
 
 
-//=================================================================================================
-/**
- * Dummy environment detection function that does not do anything. Will always fail.
- * 
- * @param callback Dummy callback function which will never be called. May be NULL.
- * @param param Parameter to be passed to the callback
- * @param __unused Unused
- * 
- * @return DSP_DETECTDUMMY_OK
- */
-//=================================================================================================
-extern u32 DSProt_DetectDummy(void* callback, void* param, u32 __unused);
-
-
-//=================================================================================================
-/**
- * Detect if the current environment is NOT a flashcart.
- * 
- * @param callback Function to be called if the environment is determined to NOT be a flashcart.
- *                 May be NULL.
- * @param param Parameter to be passed to the callback
- * @param __unused Unused
- * 
- * @return If the callback was run, the return of the callback. If a flashcart was not detected,
- *         DSP_DETECTNOTFLASHCART_OK. Otherwise, some other value.
- */
-//=================================================================================================
-extern u32 DSProt_DetectNotFlashcart(void* callback, void* param, u32 __unused);
-
-
-//=================================================================================================
-/**
- * Detect if the current environment is NOT an emulator.
- * 
- * @param callback Function to be called if the environment is determined to NOT be an emulator.
- *                 May be NULL.
- * @param param Parameter to be passed to the callback
- * @param __unused Unused
- * 
- * @return If the callback was run, the return of the callback. If an emulator was not detected,
- *         DSP_DETECTNOTEMULATOR_OK. Otherwise, some other value.
- */
-//=================================================================================================
-extern u32 DSProt_DetectNotEmulator(void* callback, void* param, u32 __unused);
-
-
-//=================================================================================================
-/**
- * DSProt_DetectNotDummy(void* callback, void* param, u32 __unused)
- * 
- * Dummy environment detection function that does not do anything. Will always succeed.
- * 
- * @param callback Dummy callback function which will always be called. May be NULL.
- * @param param Parameter to be passed to the callback
- * @param __unused Unused
- * 
- * @return If a callback was provided, the return of the callback.
- *         Otherwise, DSP_DETECTNOTDUMMY_OK.
- */
-//=================================================================================================
-extern u32 DSProt_DetectNotDummy(void* callback, void* param, u32 __unused);
-
-
-// Internal functions for emulating the old DS Protect API (many games will use this feature)
-static u32 __DSProt_DetectFlashcart_compatibilityWrapper(void* callback);
-static u32 __DSProt_DetectEmulator_compatibilityWrapper(void* callback);
-static u32 __DSProt_DetectDummy_compatibilityWrapper(void* callback);
-static u32 __DSProt_DetectNotFlashcart_compatibilityWrapper(void* callback);
-static u32 __DSProt_DetectNotEmulator_compatibilityWrapper(void* callback);
-static u32 __DSProt_DetectNotDummy_compatibilityWrapper(void* callback);
-
-
-static u32 __DSProt_DetectFlashcart_compatibilityWrapper(void* callback) {
+static u32 __DSProt_DetectFlashcart_wrapper(void* callback) {
 	if (callback) {
 		((void (*)(void))callback)();
 	}
@@ -139,7 +51,7 @@ static u32 __DSProt_DetectFlashcart_compatibilityWrapper(void* callback) {
 }
 
 
-static u32 __DSProt_DetectEmulator_compatibilityWrapper(void* callback) {
+static u32 __DSProt_DetectEmulator_wrapper(void* callback) {
 	if (callback) {
 		((void (*)(void))callback)();
 	}
@@ -147,7 +59,7 @@ static u32 __DSProt_DetectEmulator_compatibilityWrapper(void* callback) {
 }
 
 
-static u32 __DSProt_DetectDummy_compatibilityWrapper(void* callback) {
+static u32 __DSProt_DetectDummy_wrapper(void* callback) {
 	if (callback) {
 		((void (*)(void))callback)();
 	}
@@ -155,7 +67,7 @@ static u32 __DSProt_DetectDummy_compatibilityWrapper(void* callback) {
 }
 
 
-static u32 __DSProt_DetectNotFlashcart_compatibilityWrapper(void* callback) {
+static u32 __DSProt_DetectNotFlashcart_wrapper(void* callback) {
 	if (callback) {
 		((void (*)(void))callback)();
 	}
@@ -163,7 +75,7 @@ static u32 __DSProt_DetectNotFlashcart_compatibilityWrapper(void* callback) {
 }
 
 
-static u32 __DSProt_DetectNotEmulator_compatibilityWrapper(void* callback) {
+static u32 __DSProt_DetectNotEmulator_wrapper(void* callback) {
 	if (callback) {
 		((void (*)(void))callback)();
 	}
@@ -171,7 +83,7 @@ static u32 __DSProt_DetectNotEmulator_compatibilityWrapper(void* callback) {
 }
 
 
-static u32 __DSProt_DetectNotDummy_compatibilityWrapper(void* callback) {
+static u32 __DSProt_DetectNotDummy_wrapper(void* callback) {
 	if (callback) {
 		((void (*)(void))callback)();
 	}
@@ -182,7 +94,6 @@ static u32 __DSProt_DetectNotDummy_compatibilityWrapper(void* callback) {
 //=================================================================================================
 /**
  * Detect if the current environment is a flashcart.
- * Compatibility for the old DS Protect API.
  * 
  * @param callback Function to be called if the environment is determined to be a flashcart.
  *                 May be NULL.
@@ -190,15 +101,14 @@ static u32 __DSProt_DetectNotDummy_compatibilityWrapper(void* callback) {
  * @return 1 if the environment is determined to be a flashcart, 0 otherwise
  */
 //=================================================================================================
-static inline u32 DSProt_DetectFlashcart_Old(void* callback) {
-	return DSProt_DetectFlashcart(__DSProt_DetectFlashcart_compatibilityWrapper, callback, 0) == ~DSP_DETECTFLASHCART_OK;
+static inline u32 DSProt_DetectFlashcart(void* callback) {
+	return __DSProt_DetectFlashcart(__DSProt_DetectFlashcart_wrapper, callback, 0) == ~DSP_DETECTFLASHCART_OK;
 }
 
 
 //=================================================================================================
 /**
  * Detect if the current environment is an emulator.
- * Compatibility for the old DS Protect API.
  * 
  * @param callback Function to be called if the environment is determined to be an emulator.
  *                 May be NULL.
@@ -206,30 +116,28 @@ static inline u32 DSProt_DetectFlashcart_Old(void* callback) {
  * @return 1 if the environment is determined to be an emulator, 0 otherwise
  */
 //=================================================================================================
-static inline u32 DSProt_DetectEmulator_Old(void* callback) {
-	return DSProt_DetectEmulator(__DSProt_DetectEmulator_compatibilityWrapper, callback, 0) == ~DSP_DETECTEMULATOR_OK;
+static inline u32 DSProt_DetectEmulator(void* callback) {
+	return __DSProt_DetectEmulator(__DSProt_DetectEmulator_wrapper, callback, 0) == ~DSP_DETECTEMULATOR_OK;
 }
 
 
 //=================================================================================================
 /**
  * Dummy environment detection function that does not do anything. Will always fail.
- * Compatibility for the old DS Protect API.
  * 
  * @param callback Dummy callback function which will never be called. May be NULL.
  * 
  * @return 0
  */
 //=================================================================================================
-static inline u32 DSProt_DetectDummy_Old(void* callback) {
-	return DSProt_DetectDummy(__DSProt_DetectDummy_compatibilityWrapper, callback, 0) == ~DSP_DETECTDUMMY_OK;
+static inline u32 DSProt_DetectDummy(void* callback) {
+	return __DSProt_DetectDummy(__DSProt_DetectDummy_wrapper, callback, 0) == ~DSP_DETECTDUMMY_OK;
 }
 
 
 //=================================================================================================
 /**
  * Detect if the current environment is NOT a flashcart.
- * Compatibility for the old DS Protect API.
  * 
  * @param callback Function to be called if the environment is determined to NOT be a flashcart.
  *                 May be NULL.
@@ -237,15 +145,14 @@ static inline u32 DSProt_DetectDummy_Old(void* callback) {
  * @return 1 if the environment is determined to NOT be a flashcart, 0 otherwise
  */
 //=================================================================================================
-static inline u32 DSProt_DetectNotFlashcart_Old(void* callback) {
-	return DSProt_DetectNotFlashcart(__DSProt_DetectNotFlashcart_compatibilityWrapper, callback, 0) == DSP_DETECTNOTFLASHCART_OK;
+static inline u32 DSProt_DetectNotFlashcart(void* callback) {
+	return __DSProt_DetectNotFlashcart(__DSProt_DetectNotFlashcart_wrapper, callback, 0) == DSP_DETECTNOTFLASHCART_OK;
 }
 
 
 //=================================================================================================
 /**
  * Detect if the current environment is NOT an emulator.
- * Compatibility for the old DS Protect API.
  * 
  * @param callback Function to be called if the environment is determined to NOT be an emulator.
  *                 May be NULL.
@@ -253,25 +160,31 @@ static inline u32 DSProt_DetectNotFlashcart_Old(void* callback) {
  * @return 1 if the environment is determined to NOT be an emulator, 0 otherwise
  */
 //=================================================================================================
-static inline u32 DSProt_DetectNotEmulator_Old(void* callback) {
-	return DSProt_DetectNotEmulator(__DSProt_DetectNotEmulator_compatibilityWrapper, callback, 0) == DSP_DETECTNOTEMULATOR_OK;
+static inline u32 DSProt_DetectNotEmulator(void* callback) {
+	return __DSProt_DetectNotEmulator(__DSProt_DetectNotEmulator_wrapper, callback, 0) == DSP_DETECTNOTEMULATOR_OK;
 }
 
 
 //=================================================================================================
 /**
  * Dummy environment detection function that does not do anything. Will always succeed.
- * Compatibility for the old DS Protect API.
  * 
  * @param callback Dummy callback function which will always be called. May be NULL.
  * 
  * @return 1
  */
 //=================================================================================================
-static inline u32 DSProt_DetectNotDummy_Old(void* callback) {
-	return DSProt_DetectNotDummy(__DSProt_DetectNotDummy_compatibilityWrapper, callback, 0) == DSP_DETECTNOTDUMMY_OK;
+static inline u32 DSProt_DetectNotDummy(void* callback) {
+	return __DSProt_DetectNotDummy(__DSProt_DetectNotDummy_wrapper, callback, 0) == DSP_DETECTNOTDUMMY_OK;
 }
 
+
+#undef DSP_DETECTFLASHCART_OK
+#undef DSP_DETECTNOTFLASHCART_OK
+#undef DSP_DETECTEMULATOR_OK
+#undef DSP_DETECTNOTEMULATOR_OK
+#undef DSP_DETECTDUMMY_OK
+#undef DSP_DETECTNOTDUMMY_OK
 
 #ifdef __cplusplus
 }
@@ -279,12 +192,12 @@ static inline u32 DSProt_DetectNotDummy_Old(void* callback) {
 
 #else /* SDK_ASM */
 
-.public DSProt_DetectFlashcart
-.public DSProt_DetectNotFlashcart
-.public DSProt_DetectEmulator
-.public DSProt_DetectNotEmulator
-.public DSProt_DetectDummy
-.public DSProt_DetectNotDummy
+.public __DSProt_DetectFlashcart
+.public __DSProt_DetectNotFlashcart
+.public __DSProt_DetectEmulator
+.public __DSProt_DetectNotEmulator
+.public __DSProt_DetectDummy
+.public __DSProt_DetectNotDummy
 
 #endif /* SDK_ASM */
 
