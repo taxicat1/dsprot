647c1e1aeb3ce64fe4d87cf56c51990d1011fe62
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-23 15:13:43 -0400

    Add proper compare tool

---
+++ tools/compare/env.c
@@ -0,0 +1,17 @@
+// All symbols must be resolved to properly link
+#define DUMMY_FUNC(name)  \
+	void name(void);      \
+	void name(void) { }
+
+#define ENTRY  __startup
+
+DUMMY_FUNC(CARD_LockRom)
+DUMMY_FUNC(CARD_UnlockRom)
+DUMMY_FUNC(DC_StoreAll)
+DUMMY_FUNC(IC_Disable)
+DUMMY_FUNC(IC_Enable)
+DUMMY_FUNC(OS_GetLockID)
+DUMMY_FUNC(OS_GetMacAddress)
+DUMMY_FUNC(OS_GetOwnerInfo)
+
+DUMMY_FUNC(ENTRY)
