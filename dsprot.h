14566f070689d599a17f8313b2006f9841348cad
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-10-29 22:17:19 -0400

    Tweak header, add DSP_NO_NITRO

--- dsprot.h
+++ dsprot.h
@@ -1,6 +1,3 @@
-#ifndef DSPROT_H
-#define DSPROT_H
-
 //=================================================================================================
 /**
  * dsprot.h
@@ -10,13 +7,26 @@
  */
 //=================================================================================================
 
+#ifndef DSPROT_H
+#define DSPROT_H
+
 #ifndef SDK_ASM
+
+#ifndef DSP_NO_NITRO
 
 #include <nitro/types.h>  // For u32
 
+#else /* DSP_NO_NITRO */
+
+// Assumption for convenience-- make sure this is matching if you use it!
+typedef unsigned long  __dsp_u32;
+#define u32  __dsp_u32
+
+#endif /* DSP_NO_NITRO */
+
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif /* __cplusplus */
 
 // Internal DS Protect functions to be called by inlines
 extern u32 __DSProt_DetectFlashcart(void* callback, void* param, u32 __unused);
@@ -46,6 +56,10 @@ static u32 __DSProt_wrapper(void* callback) {
 extern u32 __DSProt_wrapper(void* callback);
 
 #endif /* DSP_EXT_HEADER_FUNC */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 
 //=================================================================================================
@@ -136,9 +150,11 @@ static inline u32 DSProt_DetectNotDummy(void* callback) {
 }
 
 
-#ifdef __cplusplus
-}
-#endif
+#ifdef DSP_NO_NITRO
+
+#undef u32
+
+#endif /* DSP_NO_NITRO */
 
 #else /* SDK_ASM */
 
