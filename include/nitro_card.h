e3a72123e7c3ce26f3b771bb4d1f6492f6088f80
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-12-16 18:46:07 -0500

    Pointer notation

--- include/nitro_card.h
+++ include/nitro_card.h
@@ -63,7 +63,7 @@ extern void CARDi_ReadRom(u32 dma, const void* src, void* dst, u32 len, void* ca
 extern const u8* CARD_GetRomHeader(void);
 
 
-static inline void CARD_ReadRom(u32 dma, const void *src, void *dst, u32 len) {
+static inline void CARD_ReadRom(u32 dma, const void* src, void* dst, u32 len) {
 	CARDi_ReadRom(dma, src, dst, len, NULL, NULL, FALSE);
 }
 
