01f090c04181a777bfa2eb3ae748180ebc9bdb21
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2026-01-22 17:20:39 -0500

    ROM read consistency

--- src/rom_util.c
+++ src/rom_util.c
@@ -103,7 +103,7 @@ void ROMUtil_Read(void* dest, u32 addr, s32 num_bytes) {
 			if (*(REGType32v*)(register_base + REG_CARDCNT_OFFSET) & CARD_DATA_READY) {
 				output = *(REGType32v*)(register_base + REG_CARD_DATA_OFFSET);
 				if (addr_offset >= 0 && addr_offset < num_bytes) {
-					*(u32*)(dest + addr_offset) = output;
+					*(u32*)((u32)dest + addr_offset) = output;
 				}
 				
 				addr_offset += 4;
