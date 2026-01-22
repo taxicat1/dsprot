05434e70b65628d45e80adf12b1ee029ec1c9df8
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2026-01-22 18:37:50 -0500

    Operator consistency

--- dsprot_instant.h
+++ dsprot_instant.h
@@ -82,7 +82,7 @@ static inline void* DSProt_CheckAndDetectAll(void* callback, void* param1, void*
 		// BUG: the first 5 loops have invalid shifts, resulting in 0 instead of the rotated instruction
 		func_data_checksum ^= (*func_data_ptr >> i) | (*func_data_ptr << (32-i));
 		func_data_ptr++;
-	} while (--i);
+	} while (--i != 0);
 	
 	if (func_data_checksum == DSP_EXPECTED_CHECKSUM) {
 		return DSProt_DetectAll(callback, param1, param2);
--- src/dsprot_main.c
+++ src/dsprot_main.c
@@ -74,7 +74,7 @@ static inline BOOL decryptionWrapperChecksumMatches(void* addr) {
 		// BUG: the first 5 loops have invalid shifts, resulting in 0 instead of the rotated instruction
 		checksum ^= (*func_data_ptr >> i) | (*func_data_ptr << (32-i));
 		func_data_ptr++;
-	} while (--i);
+	} while (--i != 0);
 	
 	return (checksum == DSP_EXPECTED_CHECKSUM);
 }
