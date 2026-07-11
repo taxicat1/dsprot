8e4a9201270222e1c1b35a8f7699a57f78edfb08
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2026-07-11 06:09:58 -0400

    Simplify RC4 key generation

--- src/encryptor.c
+++ src/encryptor.c
@@ -21,20 +21,17 @@ void Encryptor_StartRange(u32* addr) {
 	u8   key[16];
 	int  i;
 	u32  key_ins;
-	u8*  key_ptr;
 	u32  size;
 	
 	// First key is immediately prior to start address
 	key_ins = addr[-1];
 	
 	// Derive RC4 key
-	key_ptr = &key[0];
 	for (i = 0; i < 16; i++) {
-		*key_ptr = key_ins >> ((i % 4) * 8);
+		key[i] = key_ins >> ((i % 4) * 8);
 		if (i % 15 == 0) {
-			*key_ptr ^= 0xFF;
+			key[i] ^= 0xFF;
 		}
-		key_ptr++;
 	}
 	
 	// Search forward for second key to determine size
@@ -55,20 +52,17 @@ void Encryptor_EndRange(u32* addr) {
 	u8   key[16];
 	int  i;
 	u32  key_ins;
-	u8*  key_ptr;
 	u32  size;
 	
 	// Second key is immediately following the end address
 	key_ins = addr[1];
 	
 	// Derive RC4 key
-	key_ptr = &key[0];
 	for (i = 0; i < 16; i++) {
-		*key_ptr = key_ins >> ((i % 4) * 8);
+		key[i] = key_ins >> ((i % 4) * 8);
 		if (i % 15 == 0) {
-			*key_ptr ^= 0xFF;
+			key[i] ^= 0xFF;
 		}
-		key_ptr++;
 	}
 	
 	// Search backward for first key
